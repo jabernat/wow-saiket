43ff0a4549e02d24a68650983c4010852d1cf345
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2008-06-30 05:29:00 +0000

    _Nameplates: Tagged 1.12.4 release.

---
+++ tags/_Nameplates/1.12.4/Localization.lua
@@ -0,0 +1,17 @@
+--[[****************************************************************************
+  * _Nameplates by Saiket                                                      *
+  * Localization.lua - Localized string constants (en-US).                     *
+  ****************************************************************************]]
+
+
+_NAMEPLATES_COLUMN_LABEL_FORMAT = "Nearby %s";
+_NAMEPLATES_COLUMN_TITLES = {
+	[ "Friendly" ] = "Friendly";
+	[ "Hostile" ]  = "Hostile";
+};
+
+_NAMEPLATES_COLUMN_TOOLTIP_TITLE_FORMAT = "Nearby %s Units%s";
+_NAMEPLATES_BINDING_FORMAT = HIGHLIGHT_FONT_COLOR_CODE.." (%s)"..FONT_COLOR_CODE_CLOSE;
+
+_NAMEPLATES_COLUMN_TOOLTIP_LOCKED = "Right click to unlock.";
+_NAMEPLATES_COLUMN_TOOLTIP_UNLOCKED = "Right click to lock. Use left mouse to drag. Double click to flip text label side, and shift double click to flip the direction the column will expand towards.";
