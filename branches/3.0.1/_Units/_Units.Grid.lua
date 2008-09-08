caef12981d467232ff4b01ee0dae367bb8d23fec
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2008-09-08 06:00:03 +0000

    _Units: Made compatible with 3.0.2:
    * Optimized mouseover target tooltip.
    * Fixed Grid unit popup menus.
    * Added PitBull unit popup menus.

--- branches/3.0.1/_Units/_Units.Grid.lua
+++ branches/3.0.1/_Units/_Units.Grid.lua
@@ -7,62 +7,18 @@
 
 
 local _Units = _Units;
-local me = {
-	DropDown = nil;
-};
+local me = {};
 _Units.Grid = me;
 
 
 
 
 --[[****************************************************************************
-  * Function: _Units.Grid.InitializeGenericMenu                                *
-  * Description: Constructs the unit popup menu.                               *
-  ****************************************************************************]]
-function me.InitializeGenericMenu ()
-	local UnitID = me.DropDown.unit or "player";
-	local Which, Name, ID;
-
-	if ( UnitIsUnit( UnitID, "player" ) ) then
-		Which = "SELF";
-	elseif ( UnitIsUnit( UnitID, "pet" ) ) then
-		Which = "PET";
-	elseif ( UnitIsPlayer( UnitID ) ) then
-		ID = UnitInRaid( UnitID );
-		if ( ID ) then
-			Which = "RAID_PLAYER";
-		elseif ( UnitInParty( UnitID ) ) then
-			Which = "PARTY";
-		else
-			Which = "PLAYER";
-		end
-	else
-		Which = "RAID_TARGET_ICON";
-		Name = RAID_TARGET_ICON;
-	end
-	if ( Which ) then
-		UnitPopup_ShowMenu( me.DropDown, Which, UnitID, Name, ID );
-	end
-end
---[[****************************************************************************
-  * Function: _Units.Grid:ShowGenericMenu                                      *
-  * Description: Adds a unit popup based on the unit.                          *
-  ****************************************************************************]]
-function me:ShowGenericMenu ( unit, button, anchor )
-	HideDropDownMenu( 1 );
-
-	me.DropDown.unit = unit or SecureButton_GetUnit( self );
-
-	if ( me.DropDown.unit ) then
-		ToggleDropDownMenu( 1, nil, me.DropDown, "cursor" );
-	end
-end
---[[****************************************************************************
   * Function: _Units.Grid:GridFrameInitialize                                  *
   * Description: Hook to modify new GridFrames.                                *
   ****************************************************************************]]
 function me:GridFrameInitialize ()
-	self.menu = me.ShowGenericMenu;
+	self.menu = _Units.ShowGenericMenu;
 end
 
 
@@ -71,19 +27,11 @@ end
   * Description: Makes modifications just after the addon is loaded.           *
   ****************************************************************************]]
 function me.OnLoad ()
-	-- Create dropdown menu
-	me.DropDown = CreateFrame(
-		"Frame", "_UnitsGridDropDown", UIParent, "UIDropDownMenuTemplate" );
-	tinsert( UnitPopupFrames, me.DropDown:GetName() );
-	UIDropDownMenu_Initialize( me.DropDown, me.InitializeGenericMenu, "MENU" );
-
-
 	-- Hook Grid frames
 	hooksecurefunc( GridFrame, "InitialConfigFunction", me.GridFrameInitialize );
 
 	-- Add hook to all existing frames
-	GridFrame:WithAllFrames(
-		function ( self ) me.GridFrameInitialize( self.frame ); end );
+	GridFrame:WithAllFrames( function ( self ) me.GridFrameInitialize( self.frame ); end );
 end
 
 
