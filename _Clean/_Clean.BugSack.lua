aafbff30632fd783cec0ea3cff21ac109ef42be8
Saiket <saiket.wow@gmail.com>
2008-12-05 09:54:54 +0000

    _Clean: Updated BugSack's icon modifications from FuBar to LibDBIcon (LibDataBroker).

--- _Clean/_Clean.BugSack.lua
+++ _Clean/_Clean.BugSack.lua
@@ -15,42 +15,50 @@ _Clean.BugSack = me;
 
 
 --[[****************************************************************************
-  * Function: _Clean.BugSack.OnTextUpdate                                      *
+  * Function: _Clean.BugSack.OnUpdate                                          *
   * Description: Keeps the button disabled and transparent when empty.         *
   ****************************************************************************]]
-function me.OnTextUpdate ()
-	local Frame = BugSackFu.minimapFrame;
-
-	if ( Frame ) then
+function me.OnUpdate ()
+	if ( me.Frame ) then
 		if ( #BugSack:GetErrors( "session" ) == 0 ) then -- Clean
-			Frame:SetAlpha( 0.75 );
-			_Clean.AddLockedButton( Frame );
+			me.Frame:SetAlpha( 0.75 );
+			_Clean.AddLockedButton( me.Frame );
 		else -- Errors
-			Frame:SetAlpha( 1.0 );
-			_Clean.RemoveLockedButton( Frame );
+			me.Frame:SetAlpha( 1.0 );
+			_Clean.RemoveLockedButton( me.Frame );
 		end
 	end
 end
 --[[****************************************************************************
-  * Function: _Clean.BugSack.UpdateMinimapButton                               *
+  * Function: _Clean.BugSack.LibDBIconRegister                                 *
   * Description: Skins the minimap button when it is created.                  *
   ****************************************************************************]]
-function me.UpdateMinimapButton ( self )
-	local Frame = self.minimapFrame;
+function me:LibDBIconRegister ( Name, Object, DB )
+	if ( Name == "BugSack" ) then
+		local Frame = self.objects[ Name ];
+		me.Frame = Frame;
 
-	Frame:ClearAllPoints();
-	Frame:SetPoint( "TOPRIGHT", Minimap );
-	Frame:SetWidth( 16 );
-	Frame:SetHeight( 16 );
-	self.minimapIcon:SetAllPoints( Frame );
-	_G[ Frame:GetName().."Overlay" ]:Hide();
+		Frame:ClearAllPoints();
+		Frame:SetPoint( "TOPRIGHT", Minimap );
+		Frame:SetWidth( 16 );
+		Frame:SetHeight( 16 );
+		Frame:RegisterForDrag();
+		Frame.icon:SetAllPoints( Frame );
+		-- Hide the unnamed border texture
+		for _, Region in ipairs( { Frame:GetRegions() } ) do
+			if ( Region:IsObjectType( "Texture" ) and Region:GetTexture() == "Interface\\Minimap\\MiniMap-TrackingBorder" ) then
+				Region:Hide();
+				break;
+			end
+		end
 
-	local Background = _Clean.Colors.Background;
-	local Highlight = _Clean.Colors.Highlight;
-	self.minimapIcon:SetGradientAlpha( "VERTICAL", Highlight.r, Highlight.g, Highlight.b, Highlight.a, Background.r, Background.g, Background.b, Background.a );
+		local Background = _Clean.Colors.Background;
+		local Highlight = _Clean.Colors.Highlight;
+		Frame.icon:SetGradientAlpha( "VERTICAL", Highlight.r, Highlight.g, Highlight.b, Highlight.a, Background.r, Background.g, Background.b, Background.a );
 
-	me.OnTextUpdate();
-	Frame.SetPoint = _Clean.NilFunction;
+		--me.OnTextUpdate();
+		Frame.SetPoint = _Clean.NilFunction;
+	end
 end
 
 
@@ -62,8 +70,8 @@ end
 
 do
 	_Clean.RegisterAddOnInitializer( "BugSack", function ()
-		hooksecurefunc( BugSackFu, "Show", me.UpdateMinimapButton );
-		hooksecurefunc( BugSackFu, "OnTextUpdate", me.OnTextUpdate );
+		hooksecurefunc( LibStub( "LibDBIcon-1.0" ), "Register", me.LibDBIconRegister );
+		hooksecurefunc( BugSackLDB, "Update", me.OnUpdate );
 
 
 		-- Reskin error frame
