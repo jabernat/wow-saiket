50812a909573999f13b64d19f517588f380a9194
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2008-12-28 06:42:06 +0000

    _Units: Tagged 3.0.3.1 release.

---
+++ tags/_Units/3.0.3.1/Localization.lua
@@ -0,0 +1,35 @@
+--[[****************************************************************************
+  * _Units by Saiket                                                           *
+  * Localization.lua - Localized string constants (en-US).                     *
+  ****************************************************************************]]
+
+
+do
+
+	local Title = "_|cffcccc88Units"..FONT_COLOR_CODE_CLOSE;
+
+
+	_UnitsLocalization = setmetatable(
+		{
+			STATUSMONITOR_MANA_NOT_AVAILABLE = "N/A";
+			STATUSMONITOR_CONDITION_LABELS = {
+				[ "CORPSE" ] = "Slain";
+				[ "GHOST" ]  = "Ghost";
+				[ "ABSENT" ] = "Absent";
+				[ "FEIGN" ] = "Feign Death";
+				[ 1 ] = "CRITICAL";
+				[ 2 ] = "Poor";
+				[ 3 ] = "Fair";
+				[ 4 ] = ""; -- "Good";
+				[ 5 ] = ""; -- "Excellent";
+			};
+
+			GRID_LAYOUT_GROUP = Title..": Groups";
+			GRID_LAYOUT_CLASS = Title..": Classes";
+		}, {
+			__index = function ( self, Key )
+				rawset( self, Key, Key );
+				return Key;
+			end;
+		} );
+end
