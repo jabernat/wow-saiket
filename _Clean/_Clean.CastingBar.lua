3ae6e102287017bfbcca0094e851448ea88a7d72
Saiket <saiket.wow@gmail.com>
2009-01-16 02:47:49 +0000

    _Clean: CastingBar module now also skins the pet casting bar.

--- _Clean/_Clean.CastingBar.lua
+++ _Clean/_Clean.CastingBar.lua
@@ -1,10 +1,6 @@
 --[[****************************************************************************
   * _Clean by Saiket                                                           *
   * _Clean.CastingBar.lua - Reposition the spell casting progress bar.         *
-  *                                                                            *
-  * + Removes the casting bar's border and unnecessary highlights and flashes. *
-  * + Spans the bar between the left and right action button grids.            *
-  * + Allows spell names of any width to be shown in the bar's label.          *
   ****************************************************************************]]
 
 
@@ -12,23 +8,20 @@ local _Clean = _Clean;
 local me = {};
 _Clean.CastingBar = me;
 
-me.Overlay = CastingBarFrame:CreateTexture( nil, "OVERLAY" );
+local Bars = {};
+me.Bars = Bars;
 
 
 
 
 --[[****************************************************************************
-  * Function: _Clean.CastingBar.HideArtwork                                    *
-  * Description: This version does not have a border, has no progress spark,   *
-  *   and does not flash when done casting.                                    *
+  * Function: _Clean.CastingBar:HideArtwork                                    *
+  * Description: Hides spark, flash, and border from casting bar.              *
   ****************************************************************************]]
-local Border = CastingBarFrameBorder;
-local Flash = CastingBarFrameFlash;
-local Spark = CastingBarFrameSpark;
-function me.HideArtwork ()
-	Border:Hide();
-	Flash:Hide();
-	Spark:Hide();
+function me:HideArtwork ()
+	self.Border:Hide();
+	self.Flash:Hide();
+	self.Spark:Hide();
 end
 --[[****************************************************************************
   * Function: _Clean.CastingBar:SetStatusBarColor                              *
@@ -58,6 +51,45 @@ do
 end
 
 
+--[[****************************************************************************
+  * Function: _Clean.CastingBar:SkinCastingBar                                 *
+  * Description: Skins a casting bar frame and positions it.                   *
+  ****************************************************************************]]
+function me:SkinCastingBar ()
+	if ( not Bars[ self ] ) then
+		Bars[ self ] = true;
+
+		-- Position the bar between the left and right action bar grids
+		self:ClearAllPoints();
+		self:SetPoint( "TOPRIGHT", _Clean.ActionBar.BackdropBottomRight, "TOPLEFT", -10, -10 );
+		self:SetPoint( "BOTTOMLEFT", _Clean.ActionBar.BackdropBottomLeft, "BOTTOMRIGHT", 10, 10 );
+
+		local Text = _G[ self:GetName().."Text" ];
+		Text:ClearAllPoints();
+		Text:SetPoint( "CENTER", CastingBarFrame );
+		Text:SetWidth( 0 ); -- Allow variable width
+		Text:SetFontObject( GameFontHighlightLarge );
+
+		local Background = _Clean.Colors.Background;
+		for _, Region in ipairs( { self:GetRegions() } ) do
+			if ( Region:GetObjectType() == "Texture" and Region:GetDrawLayer() == "BACKGROUND" and Region:GetTexture() == "Solid Texture" ) then
+				Region:Hide();
+				break;
+			end
+		end
+		_Clean.Backdrop.Add( self );
+
+		-- Hooks
+		self.Border = _G[ self:GetName().."Border" ];
+		self.Flash = _G[ self:GetName().."Flash" ];
+		self.Spark = _G[ self:GetName().."Spark" ];
+		self:HookScript( "OnEvent", me.HideArtwork );
+		self:HookScript( "OnUpdate", me.HideArtwork );
+		hooksecurefunc( self, "SetStatusBarColor", me.SetStatusBarColor );
+	end
+end
+
+
 
 
 --------------------------------------------------------------------------------
@@ -65,28 +97,8 @@ end
 -----------------------------
 
 do
-	-- Position the bar between the left and right action bar grids
-	CastingBarFrame:ClearAllPoints();
-	CastingBarFrame:SetPoint( "TOPRIGHT", _Clean.ActionBar.BackdropBottomRight, "TOPLEFT", -10, -10 );
-	CastingBarFrame:SetPoint( "BOTTOMLEFT", _Clean.ActionBar.BackdropBottomLeft, "BOTTOMRIGHT", 10, 10 );
+	me.SkinCastingBar( CastingBarFrame );
+	me.SkinCastingBar( PetCastingBarFrame );
 
-	CastingBarFrameText:ClearAllPoints();
-	CastingBarFrameText:SetPoint( "CENTER", CastingBarFrame );
-	CastingBarFrameText:SetWidth( 0 ); -- Allow variable width
-	CastingBarFrameText:SetFontObject( GameFontHighlightLarge );
-
-	local Background = _Clean.Colors.Background;
-	for _, Region in ipairs( { CastingBarFrame:GetRegions() } ) do
-		if ( Region:GetObjectType() == "Texture" and Region:GetDrawLayer() == "BACKGROUND" and Region:GetTexture() == "Solid Texture" ) then
-			Region:Hide();
-			break;
-		end
-	end
-	_Clean.Backdrop.Add( CastingBarFrame );
-
-	-- Hooks
 	UIPARENT_MANAGED_FRAME_POSITIONS[ "CastingBarFrame" ] = nil;
-	CastingBarFrame:HookScript( "OnEvent", me.HideArtwork );
-	CastingBarFrame:HookScript( "OnUpdate", me.HideArtwork );
-	hooksecurefunc( CastingBarFrame, "SetStatusBarColor", me.SetStatusBarColor );
 end
