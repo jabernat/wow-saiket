1c830b7f6d010b15f572fce58852b35a6782abcb
Saiket <saiket.wow@gmail.com>
2009-01-23 00:42:27 +0000

    GridStatusHealthFade: Added 3.0.8.1 release:
    * Now compatible with the GUID version of Grid, 1.30000.2009011501.
    * Color tables are now recycled per unit to greatly reduce memory churn.
    * Removed "threshold" setting.
    * Now updates on changes to max health as well as health value.
    * Moved localization to its own file.
    * Now properly unregisters for events when disabled.

--- _Clean/GridStatusHealthFade/GridStatusHealthFade.lua
+++ _Clean/GridStatusHealthFade/GridStatusHealthFade.lua
@@ -1,153 +1,190 @@
---{{{ Libraries
+--[[****************************************************************************
+  * GridStatusHealthFade by Saiket (Originally by North101)                    *
+  * GridStatusHealthFade.lua - Adds a health status that fades from one color  *
+  *   at max health to another at no health.                                   *
+  ****************************************************************************]]
 
-local RL = AceLibrary("Roster-2.1")
-local Aura = AceLibrary("SpecialEvents-Aura-2.0")
-local L = AceLibrary("AceLocale-2.2"):new("GridStatusHealthFade")
 
---}}}
+local L = AceLibrary( "AceLocale-2.2" ):new( "GridStatusHealthFade" );
+local me = GridStatus:NewModule( "GridStatusHealthFade" );
+GridStatusHealthFade = me;
 
---{{{ Libraries
+local STATUS_ID = "alert_healthFade";
 
-L:RegisterTranslations("enUS", function()
-	return {
-		["Health Fade"] = true,
-		["HP fade threshold"] = true,
-		["Set the HP % for the HP fade."] = true,
-		["Color 2"] = true,
-		["Color 2 for HP Fade"] = true,
-	}
-end)
+local ColorTables = {};
+me.ColorTables = ColorTables;
 
---}}}
+me.menuName = L[ "Health Fade" ];
+me.options = false;
 
-GridStatusHealthFade = GridStatus:NewModule("GridStatusHealthFade")
-GridStatusHealthFade.menuName = L["Health Fade"]
-GridStatusHealthFade.options = false
+me.defaultDB = {
+	debug = false;
+	[ STATUS_ID ] = {
+		text = L[ "Health Fade" ];
+		enable = true;
+		priority = 1;
+		range = false;
+		ColorHigh = { r = 0; g = 1; b = 0; a = 1; };
+		ColorLow = { r = 1; g = 0; b = 0; a = 1; };
+	};
+};
 
---{{{ AceDB defaults
 
-GridStatusHealthFade.defaultDB = {
-	debug = false,
-	alert_healthFade = {
-		text = L["Health Fade"],
-		enable = true,
-		color = { r = 0.2, g = 0, b = 0, a = 1 },
-		color2 = { r = 1, g = 0, b = 0, a = 1 },
-		priority = 99,
-		threshold = 80,
-		range = true,
-	},
-}
 
---}}}
 
---{{{ AceOptions table
-
-local alert_healthFadeOptions = {
-	["threshold"] = {
-		type = "range",
-		name = L["HP fade threshold"],
-		desc = L["Set the HP % for the HP fade."],
-		max = 1,
-		min = 0,
-		step = 0.01,
-		isPercent = true,
-		get = function ()
-			return GridStatusHealthFade.db.profile.alert_healthFade.threshold
-		end,
-		set = function (v)
-			GridStatusHealthFade.db.profile.alert_healthFade.threshold = v
-			GridStatusHealthFade:UpdateAllUnits()
-		end,
-	},
-	["color2"] = {
-		type = "color",
-		name = L["Color 2"],
-		desc = L["Color 2 for HP Fade"],
-		hasAlpha = true,
-		get = function ()
-			local color = GridStatusHealthFade.db.profile.alert_healthFade.color2
-			return color.r, color.g, color.b, color.a
-		end,
-		set = function (r, g, b, a)
-			local color = GridStatusHealthFade.db.profile.alert_healthFade.color2
-			color.r = r
-			color.g = g
-			color.b = b
-			color.a = a or 1
-		end,
-	},
-}
-
-function GridStatusHealthFade:OnInitialize()
-	self.super.OnInitialize(self)
-	self:RegisterStatus("alert_healthFade", L["Health Fade"], alert_healthFadeOptions, true)
+--[[****************************************************************************
+  * Function: GridStatusHealthFade:OnInitialize                                *
+  * Description: Registers the HealthFade status and its options with Grid.    *
+  ****************************************************************************]]
+do
+	local Options = {
+		color = false; -- Don't use original color picker
+	};
+	local Count = 0;
+	local function AddColorPicker ( Property, Name, Description )
+		Count = Count + 1;
+		Options[ Property ] = {
+			type = "color"; hasAlpha = true; order = Count;
+			name = Name; desc = Description;
+			get = function ()
+				local Color = me.db.profile[ STATUS_ID ][ Property ];
+				return Color.r, Color.g, Color.b, Color.a;
+			end;
+			set = function ( R, G, B, A )
+				local Color = me.db.profile[ STATUS_ID ][ Property ];
+				Color.r = R;
+				Color.g = G;
+				Color.b = B;
+				Color.a = A or 1;
+				me:UpdateAllUnits();
+			end;
+		};
+	end
+	function me:OnInitialize ()
+		self.super.OnInitialize( self );
+		AddColorPicker( "ColorHigh", L[ "Color High" ], L[ "Color to blend for units at max health" ] );
+		AddColorPicker( "ColorLow", L[ "Color Low" ], L[ "Color to blend for units at no health" ] );
+		self:RegisterStatus( STATUS_ID, L[ "Health Fade" ], Options, true );
+	end
+end
+--[[****************************************************************************
+  * Function: GridStatusHealthFade:OnStatusEnable                              *
+  * Description: Called when this status is enabled and used.                  *
+  ****************************************************************************]]
+function me:OnStatusEnable ( Status )
+	if ( Status == STATUS_ID ) then
+		self:RegisterEvent( "Grid_UnitJoined", "UpdateUnit" );
+		self:RegisterEvent( "Grid_UnitLeft", "RemoveUnit" );
+		self:RegisterEvent( "Grid_UnitChanged", "UpdateUnit" );
+		self:RegisterEvent( "UNIT_HEALTH" );
+		self:RegisterEvent( "UNIT_MAXHEALTH" );
+		self:UpdateAllUnits();
+	end
+end
+--[[****************************************************************************
+  * Function: GridStatusHealthFade:OnStatusDisable                             *
+  * Description: Called when this status gets disabled or becomes unused.      *
+  ****************************************************************************]]
+function me:OnStatusDisable ( Status )
+	if ( Status == STATUS_ID ) then
+		self:UnregisterEvent( "Grid_UnitJoined" );
+		self:UnregisterEvent( "Grid_UnitLeft" );
+		self:UnregisterEvent( "Grid_UnitChanged" );
+		self:UnregisterEvent( "UNIT_HEALTH" );
+		self:UnregisterEvent( "UNIT_MAXHEALTH" );
+		self.core:SendStatusLostAllUnits( STATUS_ID );
+		wipe( ColorTables );
+	end
+end
+--[[****************************************************************************
+  * Function: GridStatusHealthFade:Reset                                       *
+  * Description: Completely reinitializes the status.                          *
+  ****************************************************************************]]
+function me:Reset ()
+	self.super.Reset( self );
+	self:OnDisable();
+	self:OnEnable();
 end
 
-function GridStatusHealthFade:OnEnable()
-	self:RegisterEvent("Grid_UnitJoined")
-	self:RegisterEvent("Grid_UnitChanged")
-	self:RegisterBucketEvent("UNIT_HEALTH", 0.2)
+
+
+
+--[[****************************************************************************
+  * Function: GridStatusHealthFade:UNIT_HEALTH                                 *
+  ****************************************************************************]]
+do
+	local UnitGUID = UnitGUID;
+	function me:UNIT_HEALTH( UnitID )
+		self:UpdateUnit( UnitGUID( UnitID ), UnitID );
+	end
 end
-
-function GridStatusHealthFade:Reset()
-	self.super.Reset(self)
-	self:UpdateAllUnits()
-end
-
-function GridStatusHealthFade:UpdateAllUnits()
-	local name, status, statusTbl
-
-	for name, status, statusTbl in self.core:CachedStatusIterator("alert_healthFade") do
-		self:Grid_UnitJoined(name)
+--[[****************************************************************************
+  * Function: GridStatusHealthFade:UNIT_MAXHEALTH                              *
+  ****************************************************************************]]
+do
+	local UnitGUID = UnitGUID;
+	function me:UNIT_MAXHEALTH( UnitID )
+		self:UpdateUnit( UnitGUID( UnitID ), UnitID );
 	end
 end
 
-function GridStatusHealthFade:UNIT_HEALTH(units)
-	local unitid
 
-	for unitid in pairs(units) do
-		self:UpdateUnit(unitid)
+--[[****************************************************************************
+  * Function: GridStatusHealthFade:RemoveUnit                                  *
+  * Description: Removes the unit's color table from the cache.                *
+  ****************************************************************************]]
+function me:RemoveUnit ( GUID, UnitID )
+	ColorTables[ GUID ] = nil;
+end
+--[[****************************************************************************
+  * Function: GridStatusHealthFade:UpdateAllUnits                              *
+  * Description: Runs UpdateUnit for all active units.                         *
+  ****************************************************************************]]
+function me:UpdateAllUnits ()
+	for GUID, UnitID in GridRoster:IterateRoster() do
+		self:UpdateUnit( GUID, UnitID );
 	end
 end
+--[[****************************************************************************
+  * Function: GridStatusHealthFade:UpdateUnit                                  *
+  * Description: Updates or removes the health fade status.                    *
+  ****************************************************************************]]
+do
+	local UnitHealth = UnitHealth;
+	local UnitHealthMax = UnitHealthMax;
+	local UnitIsDeadOrGhost = UnitIsDeadOrGhost;
+	function me:UpdateUnit( GUID, UnitID )
+		local HealthMax = UnitHealthMax( UnitID );
+		if ( HealthMax == 0 ) then -- Unit info is bogus
+			return;
+		end
+		local Percentage = UnitHealth( UnitID ) / HealthMax;
+		local Settings = self.db.profile[ STATUS_ID ];
+		local High, Low = Settings.ColorHigh, Settings.ColorLow;
 
-function GridStatusHealthFade:Grid_UnitJoined(name, unitid)
-	if unitid then
-		self:UpdateUnit(unitid, true)
-		self:UpdateUnit(unitid)
-	end
+		if ( UnitIsDeadOrGhost( UnitID ) ) then
+			self.core:SendStatusLost( GUID, STATUS_ID );
+		else
+			local Color = ColorTables[ GUID ] or {};
+			ColorTables[ GUID ] = Color;
+			Color.r = High.r * Percentage + Low.r * ( 1 - Percentage );
+			Color.g = High.g * Percentage + Low.g * ( 1 - Percentage );
+			Color.b = High.b * Percentage + Low.b * ( 1 - Percentage );
+			Color.a = High.a * Percentage + Low.a * ( 1 - Percentage );
 
-end
-
-function GridStatusHealthFade:Grid_UnitChanged(name, unitid)
-	self:UpdateUnit(unitid)
-end
-
-function GridStatusHealthFade:UpdateUnit(unitid, ignoreRange)
-	local cur, max = UnitHealth(unitid), UnitHealthMax(unitid)
-	local threshold = cur / max
-	local name = UnitName(unitid)
-	local settings = self.db.profile.alert_healthFade
-
-	if not name then return end
-
-	if threshold <= settings.threshold and
-		not UnitIsDeadOrGhost(unitid) then
-		local color = {}
-		color.r = (settings.color.r * threshold) + (settings.color2.r * (1 - threshold))
-		color.g = (settings.color.g * threshold) + (settings.color2.g * (1 - threshold))
-		color.b = (settings.color.b * threshold) + (settings.color2.b * (1 - threshold))
-		color.a = (settings.color.a * threshold) + (settings.color2.a * (1 - threshold))
-
-		self.core:SendStatusGained(name, "alert_healthFade",
-			settings.priority,
-			(settings.range and 40),
-			color,
-			math.ceil((threshold / settings.threshold) * 100).."%",
-			threshold ,
-			settings.threshold,
-			settings.icon)
-	else
-		self.core:SendStatusLost(name, "alert_healthFade")
+			-- Hack to force updates when the color changes
+			local Cache = self.core:GetCachedStatus( GUID, STATUS_ID );
+			if ( Cache ) then
+				Cache.color = nil;
+			end
+			self.core:SendStatusGained( GUID, STATUS_ID,
+				Settings.priority,
+				( Settings.range and 40 ),
+				Color,
+				L[ "%.f%%" ]:format( Percentage * 100 ),
+				Percentage,
+				1,
+				Settings.icon );
+		end
 	end
 end
