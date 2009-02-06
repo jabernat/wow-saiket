54ef441ce8631921ee448e2af923479335c1583a
Saiket <saiket.wow@gmail.com>
2009-02-06 07:20:41 +0000

    _NPCScan: Made command line interface more friendly:
    * NPC names are now saved and used case-insensitively.
    * Added a "/npcscan list" command that prints all tracked NPCs.
    * Reduced the number of colors in chat messages, and limited use of line color to errors and found alerts.
    * AddNPC and RemoveNPC APIs renamed to simply Add and Remove.

--- _NPCScan/Localization.lua
+++ _NPCScan/Localization.lua
@@ -6,26 +6,30 @@
 
 do
 	local Title = "_|cffCCCC88NPCScan|r";
-	local LDQuo, RDQuo = "\226\128\156", "\226\128\157";
+	local LDQuo, RDQuo = GRAY_FONT_COLOR_CODE.."\226\128\156", "\226\128\157"..FONT_COLOR_CODE_CLOSE;
+	local Bullet = "\226\151\143";
 	_NPCScanLocalization = setmetatable(
 		{
 			MESSAGE_FORMAT = Title..": %s";
 
-			FOUND_FORMAT = "Found "..GREEN_FONT_COLOR_CODE..LDQuo.."%s"..RDQuo..FONT_COLOR_CODE_CLOSE.."!";
+			FOUND_FORMAT = "Found "..LDQuo.."%s"..RDQuo.."!";
 			BUTTON_FOUND = "NPC found!";
 
-			NPC_ADD_FORMAT = "Added NPC "..GRAY_FONT_COLOR_CODE..LDQuo.."%s"..RDQuo..FONT_COLOR_CODE_CLOSE.." (ID %d).";
-			NPC_REMOVE_FORMAT = "Removed NPC "..GRAY_FONT_COLOR_CODE..LDQuo.."%s"..RDQuo..FONT_COLOR_CODE_CLOSE.." (ID %d).";
+			NPC_ADD_FORMAT = "Added NPC "..LDQuo.."%s"..RDQuo.." (ID %d).";
+			NPC_REMOVE_FORMAT = "Removed NPC "..LDQuo.."%s"..RDQuo.." (ID %d).";
 
 			ALREADY_CACHED_FORMAT = "Consider clearing your cache to reset the following units: %s.";
-			NAME_FORMAT = GRAY_FONT_COLOR_CODE..LDQuo.."%s"..RDQuo..FONT_COLOR_CODE_CLOSE;
+			NAME_FORMAT = LDQuo.."%s"..RDQuo;
 			NAME_SEPARATOR = ", ";
 
+			CMD_LIST = "LIST";
+			CMD_LIST_FORMAT = "Listing %d |4unit:units;:";
+			CMD_LISTENTRY_FORMAT = "    "..Bullet.." "..LDQuo.."%s"..RDQuo.." (ID %d)";
 			CMD_ADD = "ADD";
-			CMD_ADDDUPLICATE_FORMAT = "NPC "..GRAY_FONT_COLOR_CODE..LDQuo.."%s"..RDQuo..FONT_COLOR_CODE_CLOSE.." (ID %d) already being searched for.";
+			CMD_ADDDUPLICATE_FORMAT = "NPC "..LDQuo.."%s"..RDQuo.." (ID %d) already being searched for.";
 			CMD_REMOVE = "REMOVE";
-			CMD_REMOVENOTFOUND_FORMAT = "NPC "..GRAY_FONT_COLOR_CODE..LDQuo.."%s"..RDQuo..FONT_COLOR_CODE_CLOSE.." not found. (Case sensitive!)";
-			CMD_HELP = "Commands are "..GRAY_FONT_COLOR_CODE..LDQuo.."/npcscan add "..GREEN_FONT_COLOR_CODE.."<NpcID> <Name>"..GRAY_FONT_COLOR_CODE..RDQuo..FONT_COLOR_CODE_CLOSE.." and "..GRAY_FONT_COLOR_CODE..LDQuo.."/npcscan remove "..GREEN_FONT_COLOR_CODE.."<Name>"..GRAY_FONT_COLOR_CODE..RDQuo..FONT_COLOR_CODE_CLOSE..".";
+			CMD_REMOVENOTFOUND_FORMAT = "NPC "..LDQuo.."%s"..RDQuo.." not found.";
+			CMD_HELP = "Commands are "..LDQuo.."/npcscan list"..RDQuo..", "..LDQuo.."/npcscan add <NpcID> <Name>"..RDQuo..", and "..LDQuo.."/npcscan remove <Name>"..RDQuo..".";
 		}, {
 			__index = function ( self, Key )
 				rawset( self, Key, Key );
