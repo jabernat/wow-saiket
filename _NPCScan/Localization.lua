d49fd99b6eed384331f363ceabcd72593de76f4f
Saiket <saiket.wow@gmail.com>
2009-02-26 04:34:37 +0000

    _NPCScan: Removed the slash command interface and added a GUI panel to the Interface Options menu:
    * Now embeds LibTextTable-1.0 to generate a list of NPCs.
    * The options window includes a "Test" button that displays a mock "NPC Found" alert and prints a short set of instructions.

--- _NPCScan/Localization.lua
+++ _NPCScan/Localization.lua
@@ -7,7 +7,6 @@
 do
 	local Title = "_|cffCCCC88NPCScan|r";
 	local LDQuo, RDQuo = GRAY_FONT_COLOR_CODE.."\226\128\156", "\226\128\157"..FONT_COLOR_CODE_CLOSE;
-	local Bullet = "\226\151\143";
 	_NPCScanLocalization = setmetatable(
 		{
 			MESSAGE_FORMAT = Title..": %s";
@@ -15,21 +14,30 @@ do
 			FOUND_FORMAT = "Found "..LDQuo.."%s"..RDQuo.."!";
 			BUTTON_FOUND = "NPC found!";
 
-			NPC_ADD_FORMAT = "Added NPC "..LDQuo.."%s"..RDQuo.." (ID %d).";
-			NPC_REMOVE_FORMAT = "Removed NPC "..LDQuo.."%s"..RDQuo.." (ID %d).";
-
 			ALREADY_CACHED_FORMAT = "Consider clearing your cache to reset the following units: %s.";
 			NAME_FORMAT = LDQuo.."%s"..RDQuo;
 			NAME_SEPARATOR = ", ";
 
-			CMD_LIST = "LIST";
-			CMD_LIST_FORMAT = "Listing %d |4unit:units;:";
-			CMD_LISTENTRY_FORMAT = "    "..Bullet.." "..LDQuo.."%s"..RDQuo.." (ID %d)";
-			CMD_ADD = "ADD";
-			CMD_ADDDUPLICATE_FORMAT = "NPC "..LDQuo.."%s"..RDQuo.." (ID %d) already being searched for.";
-			CMD_REMOVE = "REMOVE";
-			CMD_REMOVENOTFOUND_FORMAT = "NPC "..LDQuo.."%s"..RDQuo.." not found.";
-			CMD_HELP = "Commands are "..LDQuo.."/npcscan list"..RDQuo..", "..LDQuo.."/npcscan add <NpcID> <Name>"..RDQuo..", and "..LDQuo.."/npcscan remove <Name>"..RDQuo..".";
+
+			OPTIONS_TITLE = Title;
+			OPTIONS_DESC = "This table allows you to add or remove NPCs to scan for.  Note that tameable mobs cannot be searched for since they get found even as hunter pets.";
+
+			OPTIONS_TEST = "Test Found Alert";
+			OPTIONS_TEST_DESC = "Simulates an "..LDQuo.."NPC found"..RDQuo.." alert to let you know what to look out for.";
+			OPTIONS_TEST_NAME = "You! (Test)";
+			OPTIONS_TEST_HELP_FORMAT = "Click the target button or use the provided keybinding to target the found mob.  Hold "..HIGHLIGHT_FONT_COLOR_CODE.."<%s>"..FONT_COLOR_CODE_CLOSE.." and drag to move the target button.  Note that if an NPC is found while you're in combat, the button will only appear after you exit combat.";
+
+			OPTIONS_CACHED = "Found";
+			OPTIONS_NAME = "Name:";
+			OPTIONS_NAME_DESC = "A label for the NPC.  It is case insensitive and doesn't have to match the NPC's actual name.";
+			OPTIONS_ID = "NPC ID:";
+			OPTIONS_ID_DESC = "The ID of the NPC to search for.  This value can be found on sites like WowHead.com.";
+
+			OPTIONS_CACHED_YES = "|T"..READY_CHECK_NOT_READY_TEXTURE..":0|t";
+			OPTIONS_CACHED_NO = "";
+
+			OPTIONS_ADD = "+";
+			OPTIONS_REMOVE = "-";
 		}, {
 			__index = function ( self, Key )
 				rawset( self, Key, Key );
