907ac371b3cb2bcd4e1f7a16afa3b1846f498b42
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2009-03-30 20:56:17 +0000

    _Corpse: Tagged 3.0.9.4 release.

---
+++ tags/_Corpse/3.0.9.4/Locales/Locale-enUS.lua
@@ -0,0 +1,34 @@
+--[[****************************************************************************
+  * _Corpse by Saiket                                                          *
+  * Locales/Locale-enUS.lua - Localized string constants (en-US).              *
+  ****************************************************************************]]
+
+
+do
+	_CorpseLocalization = setmetatable( {
+		CORPSE_PATTERN = "^Corpse of ([^ ]+)$"; -- Must also catch cross-realm names based on CORPSE_TOOLTIP
+		CORPSE_FORMAT = CORPSE_TOOLTIP;
+		SERVER_DELIMITER = "-";
+
+		LEVEL_CLASS_PATTERN = FRIENDS_LEVEL_TEMPLATE;
+		ONLINE = GUILD_ONLINE_LABEL;
+		OFFLINE = PLAYER_OFFLINE;
+
+		FRIEND_ADDED_PATTERN = "^([^%s%p%d%c]+) added to friends%.$"; -- Based on ERR_FRIEND_ADDED_S
+		FRIEND_REMOVED_PATTERN = "^([^%s%p%d%c]+) removed from friends list%.$"; -- Based on ERR_FRIEND_REMOVED_S
+		FRIEND_IS_ENEMY = ERR_FRIEND_WRONG_FACTION;
+
+		ENEMY_ONLINE = ERR_PLAYER_WRONG_FACTION;
+		ENEMY_OFFLINE_PATTERN = "^Cannot find player '([^%s%p%d%c]+)'%.$"; -- Based on ERR_BAD_PLAYER_NAME_S
+
+		AFK = CHAT_FLAG_AFK;
+		DND = CHAT_FLAG_DND;
+	}, {
+		__index = function ( self, Key )
+			if ( Key ~= nil ) then
+				rawset( self, Key, Key );
+				return Key;
+			end
+		end;
+	} );
+end
