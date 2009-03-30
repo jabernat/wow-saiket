496a65457c4bca5321644d71f00f16b80f9d590e
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2009-03-30 20:55:30 +0000

    _Corpse: Fixed Chinese localizations to exactly match GlobalStrings formats.

--- trunk/_Corpse/Locales/Locale-zhCN.lua
+++ trunk/_Corpse/Locales/Locale-zhCN.lua
@@ -8,9 +8,9 @@ if ( GetLocale() == "zhCN" ) then
 	_CorpseLocalization = setmetatable( {
 		CORPSE_PATTERN = "^([^ ]+)的尸体$"; -- Must also catch cross-realm names based on CORPSE_TOOLTIP
 
-		FRIEND_ADDED_PATTERN = "^([^%s%p%d%c]+) 已被添加到好友名单%.$"; -- Based on ERR_FRIEND_ADDED_S
-		FRIEND_REMOVED_PATTERN = "^([^%s%p%d%c]+) 已从好友名单中移除%.$"; -- Based on ERR_FRIEND_REMOVED_S
+		FRIEND_ADDED_PATTERN = "^([^%s%p%d%c]+)已被加入好友名单$"; -- Based on ERR_FRIEND_ADDED_S
+		FRIEND_REMOVED_PATTERN = "^([^%s%p%d%c]+)已被从好友名单中删除$"; -- Based on ERR_FRIEND_REMOVED_S
 
-		ENEMY_OFFLINE_PATTERN = "^找不到名为 '([^%s%p%d%c]+)' 的玩家%.$"; -- Based on ERR_BAD_PLAYER_NAME_S
+		ENEMY_OFFLINE_PATTERN = "^无法找到玩家'([^%s%p%d%c]+)'。$"; -- Based on ERR_BAD_PLAYER_NAME_S
 	}, { __index = _CorpseLocalization; } );
 end
