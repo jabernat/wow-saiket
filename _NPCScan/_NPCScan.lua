8c433808e671b4e47045a84fde0af904c6b8b099
Saiket <saiket.wow@gmail.com>
2009-04-12 00:47:48 +0000

    _NPCScan: Added preliminary support for achievement-based NPC tracking.  Because of the change, only custom NPCs like the Time-Lost Proto Drake require localizing.

--- _NPCScan/_NPCScan.lua
+++ _NPCScan/_NPCScan.lua
@@ -17,62 +17,33 @@ me.OptionsDefault = {
 };
 me.OptionsCharacterDefault = {
 	Version = me.Version;
-	IDs = { -- Keys must be lowercase and trimmed, but don't have to match the NPC name
+	NPCs = { -- Keys must be lowercase and trimmed, but don't have to match the NPC name
 		-- Note: Tameable NPCs will be "found" if you encounter them as pets, so don't search for them.
-
-		[ L[ "Time-Lost Proto Drake" ]:lower() ] = 32491;
-
-		-- Northern Exposure (Northrend)
-		[ L[ "Aotona" ]:lower() ] = 32481;
-		[ L[ "Dirkee" ]:lower() ] = 32500;
-		[ L[ "Griegen" ]:lower() ] = 32471;
-		[ L[ "High Thane Jorfus" ]:lower() ] = 32501;
-		[ L[ "Icehorn" ]:lower() ] = 32361;
-		[ L[ "King Ping" ]:lower() ] = 32398;
-		[ L[ "Old Crystalbark" ]:lower() ] = 32357;
-		[ L[ "Putridus the Ancient" ]:lower() ] = 32487;
-		[ L[ "Seething Hate" ]:lower() ] = 32429;
-		[ L[ "Terror Spinner" ]:lower() ] = 32475;
-		[ L[ "Vigdis the War Maiden" ]:lower() ] = 32386;
-		[ L[ "Zul'drak Sentinel" ]:lower() ] = 32447;
-		[ L[ "Crazed Indu'le Survivor" ]:lower() ] = 32409;
-		[ L[ "Fumblub Gearwind" ]:lower() ] = 32358;
-		[ L[ "Grocklar" ]:lower() ] = 32422;
-		[ L[ "Hildana Deathstealer" ]:lower() ] = 32495;
-		--[ L[ "King Krush" ]:lower() ] = 32485;
-		--[ L[ "Loque'nahak" ]:lower() ] = 32517;
-		[ L[ "Perobas the Bloodthirster" ]:lower() ] = 32377;
-		[ L[ "Scarlet Highlord Daion" ]:lower() ] = 32417;
-		[ L[ "Syreian the Bonecarver" ]:lower() ] = 32438;
-		[ L[ "Tukemuth" ]:lower() ] = 32400;
-		[ L[ "Vyragosa" ]:lower() ] = 32630;
-
-		-- Bloody Rare (Outlands)
-		[ L[ "Ambassador Jerrikar" ]:lower() ] = 18695;
-		[ L[ "Chief Engineer Lorthander" ]:lower() ] = 18697;
-		[ L[ "Collidus the Warp-Watcher" ]:lower() ] = 18694;
-		[ L[ "Doomsayer Jurim" ]:lower() ] = 18686;
-		[ L[ "Fulgorge" ]:lower() ] = 18678;
-		[ L[ "Hemathion" ]:lower() ] = 18692;
-		[ L[ "Marticar" ]:lower() ] = 18680;
-		[ L[ "Morcrush" ]:lower() ] = 18690;
-		[ L[ "Okrek" ]:lower() ] = 18685;
-		[ L[ "Voidhunter Yar" ]:lower() ] = 18683;
-		[ L[ "Bog Lurker" ]:lower() ] = 18682;
-		[ L[ "Coilfang Emissary" ]:lower() ] = 18681;
-		[ L[ "Crippler" ]:lower() ] = 18689;
-		[ L[ "Ever-Core the Punisher" ]:lower() ] = 18698;
-		--[ L[ "Goretooth" ]:lower() ] = 17144;
-		[ L[ "Kraator" ]:lower() ] = 18696;
-		[ L[ "Mekthorg the Wild" ]:lower() ] = 18677;
-		--[ L[ "Nuramoc" ]:lower() ] = 20932;
-		[ L[ "Speaker Mar'grom" ]:lower() ] = 18693;
-		[ L[ "Vorakem Doomspeaker" ]:lower() ] = 18679;
+		[ L.NPCS[ "Time-Lost Proto Drake" ]:trim():lower() ] = 32491;
 	};
+	Achievements = {}; -- Filled with all entries in me.Achievements
+	AchievementsAddFound = false;
 };
 
 
+me.TamableIDs = {
+	-- Bloody Rare (Outlands)
+	[ 17144 ] = true; -- Goretooth
+	[ 20932 ] = true; -- Nuramoc
+	-- Frostbitten (Northrend)
+	[ 32485 ] = true; -- King Krush
+	[ 32517 ] = true; -- Loque'nahak
+};
+
 me.ScanIDs = {}; -- [ NPC ID ] = Number of concurrent scans for this ID
+me.ScanGroups = setmetatable( {}, { -- Subsets of scanned NPCs divided by group ("NPC" or AchievementID)
+	__index = function ( self, Group ) local Table = {}; rawset( self, Group, Table ); return Table; end;
+} );
+
+me.Achievements = { -- Criteria data for each achievement
+	[ 1312 ] = {}; -- Bloody Rare (Outlands)
+	[ 2257 ] = {}; -- Frostbitten (Northrend)
+};
 
 me.IDMax = 0xFFFF; -- Largest ID that will fit in a GUID's 2-byte NPC ID field
 me.UpdateRate = 0.1;
@@ -125,89 +96,163 @@ end
   * Function: _NPCScan.ScanAdd                                                 *
   * Description: Begins searching for an NPC ID.                               *
   ****************************************************************************]]
-function me.ScanAdd ( ID )
-	me.ScanIDs[ ID ] = ( me.ScanIDs[ ID ] or 0 ) + 1; -- Increment
+function me.ScanAdd ( Group, ID )
+	Group = me.ScanGroups[ Group ];
+
+	-- Increment both group and total
+	Group[ ID ] = ( Group[ ID ] or 0 ) + 1;
+	me.ScanIDs[ ID ] = ( me.ScanIDs[ ID ] or 0 ) + 1;
 end
 --[[****************************************************************************
   * Function: _NPCScan.ScanRemove                                              *
   * Description: Stops searching for an NPC ID.                                *
   ****************************************************************************]]
-function me.ScanRemove ( ID, All )
-	if ( All ) then -- Stop all concurrent scans
-		me.ScanIDs[ ID ] = nil;
-	else -- Stop only one instance of the scan
-		local Count = me.ScanIDs[ ID ];
-		if ( Count ) then -- Reduce counter, and clear if zero
-			me.ScanIDs[ ID ] = Count > 1 and Count - 1 or nil;
-		end
+function me.ScanRemove ( Group, ID )
+	local Removed = false;
+	Group = me.ScanGroups[ Group ];
+
+	-- Remove from group
+	local Count = Group[ ID ];
+	if ( Count ) then -- Reduce counter, and clear if zero
+		Group[ ID ] = Count > 1 and Count - 1 or nil;
+		Removed = true;
+	end
+
+	if ( Removed ) then -- Was present in group; Decrement total
+		me.ScanIDs[ ID ] = me.ScanIDs[ ID ] > 1 and me.ScanIDs[ ID ] - 1 or nil;
 	end
 end
-
-
 --[[****************************************************************************
-  * Function: _NPCScan.Add                                                     *
-  * Description: Adds an NPC name and ID to settings and begins searching.     *
+  * Function: _NPCScan.ScanRemoveAll                                           *
+  * Description: Stops all concurrent scans for a common NPC ID.               *
   ****************************************************************************]]
-function me.Add ( Name, ID )
-	Name = Name:trim():lower();
-	ID = tonumber( ID );
-
-	local OldID = _NPCScanOptionsCharacter.IDs[ Name ];
-	if ( OldID == ID ) then
-		return; -- No change
-	elseif ( OldID ) then -- Replace old value
-		me.ScanRemove( OldID );
+function me.ScanRemoveAll ( ID )
+	for _, Group in pairs( me.ScanGroups ) do
+		Group[ ID ] = nil;
 	end
-	_NPCScanOptionsCharacter.IDs[ Name ] = ID;
-
-	local FoundName = me.TestID( ID );
-	if ( FoundName ) then -- Already seen
-		return true, FoundName;
-	else
-		me.ScanAdd( ID );
-		return true;
-	end
+	me.ScanIDs[ ID ] = nil;
 end
 --[[****************************************************************************
-  * Function: _NPCScan.Remove                                                  *
-  * Description: Removes an NPC from settings by name and stops searching.     *
-  ****************************************************************************]]
-function me.Remove ( Name )
-	Name = Name:trim():lower();
-	local ID = _NPCScanOptionsCharacter.IDs[ Name ];
-
-	if ( ID ) then
-		_NPCScanOptionsCharacter.IDs[ Name ] = nil;
-		me.ScanRemove( ID );
-
-		return true;
-	end
-end
---[[****************************************************************************
-  * Function: _NPCScan.SynchronizeIDs                                          *
+  * Function: _NPCScan.ScanSynchronize                                         *
   * Description: Resets the scanning list and reloads it from saved settings.  *
   ****************************************************************************]]
 do
 	local CachedNames = {};
-	function me.SynchronizeIDs ()
-		wipe( me.ScanIDs ); -- Remove old scanned mobs
+	function me.ScanSynchronize ()
+		-- Clear all scans
+		for ID in pairs( me.ScanIDs ) do
+			me.ScanRemoveAll( ID );
+		end
 
 		-- Add all NPCs from options
-		for Name, ID in pairs( _NPCScanOptionsCharacter.IDs ) do
-			-- Don't add NPCs already in the cache
-			local FoundName = me.TestID( ID );
-			if ( FoundName ) then
+		for Name, ID in pairs( _NPCScanOptionsCharacter.NPCs ) do
+			_NPCScanOptionsCharacter.NPCs[ Name ] = nil;
+			local Success, FoundName = me.NPCAdd( Name, ID );
+			if ( Success and FoundName ) then -- Was already cached
 				CachedNames[ #CachedNames + 1 ] = L.NAME_FORMAT:format( FoundName );
-			else -- Add
-				me.ScanAdd( ID );
 			end
 		end
-		-- Print all cached names
+		-- Print all cached NPC names
 		if ( next( CachedNames ) ) then
 			table.sort( CachedNames );
 			me.Message( L.ALREADY_CACHED_FORMAT:format( table.concat( CachedNames, L.NAME_SEPARATOR ) ) );
 			wipe( CachedNames );
 		end
+
+		for AchievementID in pairs( me.Achievements ) do
+			if ( _NPCScanOptionsCharacter.Achievements[ AchievementID ] ) then
+				_NPCScanOptionsCharacter.Achievements[ AchievementID ] = nil;
+				local Success, FoundList = me.AchievementAdd( AchievementID );
+				if ( Success and FoundList ) then -- Some NPCs were already cached
+					me.Message( L.ALREADY_CACHED_FORMAT:format( FoundList ) );
+				end
+			end
+		end
+	end
+end
+
+
+--[[****************************************************************************
+  * Function: _NPCScan.AchievementAdd                                          *
+  * Description: Adds a kill-related achievement to track.                     *
+  ****************************************************************************]]
+do
+	local CachedNames = {};
+	function me.AchievementAdd ( AchievementID )
+		if ( not _NPCScanOptionsCharacter.Achievements[ AchievementID ] ) then
+			_NPCScanOptionsCharacter.Achievements[ AchievementID ] = true;
+
+			for ID, CriteriaID in pairs( me.Achievements[ AchievementID ].NPCs ) do
+				local _, CriteriaType, Completed = GetAchievementCriteriaInfo( CriteriaID );
+				if ( not Completed or _NPCScanOptionsCharacter.AchievementsAddFound ) then
+					local FoundName = me.TestID( ID );
+					if ( FoundName ) then -- Already seen
+						CachedNames[ #CachedNames + 1 ] = L.NAME_FORMAT:format( FoundName );
+					else
+						me.ScanAdd( AchievementID, ID );
+					end
+				end
+			end
+
+			local CachedList;
+			if ( next( CachedNames ) ) then -- Return all cached names
+				table.sort( CachedNames );
+				CachedList = table.concat( CachedNames, L.NAME_SEPARATOR );
+				wipe( CachedNames );
+			end
+			return true, CachedList;
+		end
+	end
+end
+--[[****************************************************************************
+  * Function: _NPCScan.AchievementRemove                                       *
+  * Description: Removes an achievement from settings and stops tracking it.   *
+  ****************************************************************************]]
+function me.AchievementRemove ( AchievementID )
+	if ( _NPCScanOptionsCharacter.Achievements[ AchievementID ] ) then
+		_NPCScanOptionsCharacter.Achievements[ AchievementID ] = nil;
+
+		for ID in pairs( me.Achievements[ AchievementID ].NPCs ) do
+			me.ScanRemove( AchievementID, ID );
+		end
+		return true;
+	end
+end
+
+
+--[[****************************************************************************
+  * Function: _NPCScan.NPCAdd                                                  *
+  * Description: Adds an NPC name and ID to settings and begins searching.     *
+  ****************************************************************************]]
+function me.NPCAdd ( Name, ID )
+	Name = Name:trim():lower();
+	ID = tonumber( ID );
+
+	if ( not _NPCScanOptionsCharacter.NPCs[ Name ] ) then
+		_NPCScanOptionsCharacter.NPCs[ Name ] = ID;
+
+		local FoundName = me.TestID( ID );
+		if ( FoundName ) then -- Already seen
+			return true, FoundName;
+		else
+			me.ScanAdd( "NPC", ID );
+			return true;
+		end
+	end
+end
+--[[****************************************************************************
+  * Function: _NPCScan.NPCRemove                                               *
+  * Description: Removes an NPC from settings by name and stops searching.     *
+  ****************************************************************************]]
+function me.NPCRemove ( Name )
+	Name = Name:trim():lower();
+	local ID = _NPCScanOptionsCharacter.NPCs[ Name ];
+
+	if ( ID ) then
+		_NPCScanOptionsCharacter.NPCs[ Name ] = nil;
+		me.ScanRemove( "NPC", ID );
+
+		return true;
 	end
 end
 
@@ -230,48 +275,45 @@ do
 				if ( Name ) then
 					me.Alert( L.FOUND_FORMAT:format( Name ), GREEN_FONT_COLOR );
 					me.Button.SetNPC( Name, ID );
-					me.ScanRemove( ID, true ); -- Stop searching for this NPC entirely
+					me.ScanRemoveAll( ID );
 				end
 			end
 		end
 	end
 end
 --[[****************************************************************************
-  * Function: _NPCScan:ADDON_LOADED                                            *
+  * Function: _NPCScan.OnLoad                                                  *
+  * Description: Loads defaults, validates settings, and starts scan.          *
   ****************************************************************************]]
-function me:ADDON_LOADED ( _, AddOn )
-	if ( AddOn:upper() == "_NPCSCAN" ) then
-		me:UnregisterEvent( "ADDON_LOADED" );
-		me.ADDON_LOADED = nil;
+function me.OnLoad ()
+	me.OnLoad = nil;
 
-		-- Apply default settings
-		if ( not _NPCScanOptions ) then
+	-- Apply default settings
+	if ( not _NPCScanOptions ) then
+		_NPCScanOptions = CopyTable( me.OptionsDefault );
+	end
+	if ( not _NPCScanOptionsCharacter ) then
+		_NPCScanOptionsCharacter = CopyTable( me.OptionsCharacterDefault );
+	end
+
+	-- Validate settings
+	if ( _NPCScanOptionsCharacter.Version ~= me.Version ) then
+		if ( _NPCScanOptions.Version ~= me.Version ) then
 			_NPCScanOptions = CopyTable( me.OptionsDefault );
 		end
-		if ( not _NPCScanOptionsCharacter ) then
-			_NPCScanOptionsCharacter = CopyTable( me.OptionsCharacterDefault );
-		end
-
-		-- Validate settings
-		if ( _NPCScanOptions.Version ~= me.Version ) then
-			if ( _NPCScanOptions.Version == nil ) then -- Pre 3.0.9.2
-				-- NPC IDs need to be moved into per-character storage
-				_NPCScanOptionsCharacter.IDs = _NPCScanOptions.IDs;
-				_NPCScanOptions = CopyTable( me.OptionsDefault );
-			end
-			_NPCScanOptions.Version = me.Version;
-		end
-		if ( _NPCScanOptionsCharacter.Version ~= me.Version ) then
-			_NPCScanOptionsCharacter.Version = me.Version;
-		end
-
-		me.SynchronizeIDs();
+		_NPCScanOptionsCharacter = CopyTable( me.OptionsCharacterDefault );
 	end
+
+	me.ScanSynchronize();
 end
 --[[****************************************************************************
   * Function: _NPCScan:PLAYER_ENTERING_WORLD                                   *
   ****************************************************************************]]
 function me:PLAYER_ENTERING_WORLD ()
+	if ( me.OnLoad ) then -- Only run once
+		me.OnLoad();
+	end
+
 	-- Do not scan while in instances
 	if ( IsInInstance() ) then
 		self:Hide();
@@ -281,7 +323,6 @@ function me:PLAYER_ENTERING_WORLD ()
 end
 --[[****************************************************************************
   * Function: _NPCScan:OnEvent                                                 *
-  * Description: Global event handler.                                         *
   ****************************************************************************]]
 do
 	local type = type;
@@ -302,7 +343,6 @@ end
 do
 	me:SetScript( "OnUpdate", me.OnUpdate );
 	me:SetScript( "OnEvent", me.OnEvent );
-	me:RegisterEvent( "ADDON_LOADED" );
 	me:RegisterEvent( "PLAYER_ENTERING_WORLD" );
 
 	-- Add template text lines
@@ -310,4 +350,19 @@ do
 	Tooltip:AddFontStrings(
 		Tooltip.Text,
 		Tooltip:CreateFontString( "$parentTextRight1", nil, "GameTooltipText" ) );
+
+
+	-- Save achievement criteria data
+	for AchievementID, Table in pairs( me.Achievements ) do
+		me.OptionsCharacterDefault.Achievements[ AchievementID ] = true;
+		Table.Name = select( 2, GetAchievementInfo( AchievementID ) );
+
+		Table.NPCs = {};
+		for Criteria = 1, GetAchievementNumCriteria( AchievementID ) do
+			local _, CriteriaType, _, _, _, _, _, AssetID, _, CriteriaID = GetAchievementCriteriaInfo( AchievementID, Criteria );
+			if ( CriteriaType == 0 and not me.TamableIDs[ AssetID ] ) then -- Mob kill type
+				Table.NPCs[ AssetID ] = CriteriaID;
+			end
+		end
+	end
 end
