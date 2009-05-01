c3bfcee5a43df669923c0b6f371fe3072fa4f4ed
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2009-05-01 00:38:46 +0000

    _Clean: Gladius module brought up to date.

--- trunk/_Clean/_Clean.Gladius.lua
+++ trunk/_Clean/_Clean.Gladius.lua
@@ -92,72 +92,87 @@ end
 
 
 
+--[[****************************************************************************
+  * Function: _Clean.Gladius:CreateButton                                      *
+  ****************************************************************************]]
+function me:CreateButton ( Index )
+	local Button = _G[ "GladiusButtonFrame"..Index ];
+
+	-- Black backdrop
+	local Background = Button:CreateTexture( nil, "BACKGROUND" );
+	Background:SetTexture( 0, 0, 0 );
+	Background:SetPoint( "TOPRIGHT", Button.secure, 2, 2 );
+	Background:SetPoint( "BOTTOMLEFT", Button.secure, -2, -4 );
+
+	-- Left icon
+	Button.classIcon:SetVertexColor( 0.25, 0.25, 0.25 );
+	_Clean.RemoveButtonIconBorder( Button.auraFrame.icon );
+
+	-- Fade bar backgrounds
+	Button.health.bg:SetAlpha( 0.05 );
+	Button.mana.bg:SetAlpha( 0.1 );
+	Button.castBar.bg:SetAlpha( 0.05 );
+
+	-- Use "healer mode" reverse bars that blend colors
+	local Health = Button.health;
+	Health.Text = Button.healthText;
+	Health.Texture = Health:CreateTexture( nil, "ARTWORK" );
+	Health.Texture:SetPoint( "TOPRIGHT" );
+	Health.Texture:SetPoint( "BOTTOM" );
+	Health.Texture:SetTexture( Health:GetStatusBarTexture():GetTexture() );
+	Health:SetStatusBarTexture( nil );
+	Health.SetStatusBarTexture  = me.HealthSetStatusBarTexture;
+	_Clean.HookScript( Health, "OnValueChanged", me.HealthOnValueChanged );
+	_Clean.HookScript( Health, "OnSizeChanged", me.HealthOnSizeChanged );
+
+	-- Use class color for name
+	hooksecurefunc( Health, "SetStatusBarColor", me.HealthSetStatusBarColor );
+	Button.text.SetTextColorBackup = Button.text.SetTextColor;
+	Button.text.SetTextColor = _Clean.NilFunction;
+
+	-- Color mana text like mana bar
+	hooksecurefunc( Button.mana, "SetStatusBarColor", me.ManaSetStatusBarColor );
+	Button.manaText.SetTextColorBackup = Button.manaText.SetTextColor;
+	Button.manaText.SetTextColor = _Clean.NilFunction;
+
+	-- Remove original highlight and use mouseover highlight
+	local Secure = Button.secure; -- Actual unit button
+	Secure.Highlight = Secure:CreateTexture( nil, "OVERLAY" );
+	Secure.Highlight:Hide();
+	Secure.Highlight:SetAllPoints( Background );
+	Secure.Highlight:SetBlendMode( "ADD" );
+	Secure.Highlight:SetTexture( Button.highlight:GetTexture() );
+	Button.highlight:SetTexture( nil );
+	_Clean.HookScript( Secure, "OnEnter", me.OnEnter );
+	_Clean.HookScript( Secure, "OnLeave", me.OnLeave );
+
+	-- Remove ranks from casting text
+	local SpellText = Button.castBar.spellText;
+	SpellText.SetTextBackup = SpellText.SetText;
+	SpellText.SetText = me.SpellSetText;
+end
+--[[****************************************************************************
+  * Function: _Clean.Gladius:CreateFrame                                       *
+  ****************************************************************************]]
+function me:CreateFrame ()
+	Gladius.frame:SetBackdrop( {} );
+	Gladius.frame:EnableMouse( false );
+end
+
+
+
+
 --------------------------------------------------------------------------------
 -- Function Hooks / Execution
 -----------------------------
 
 do
 	_Clean.RegisterAddOnInitializer( "Gladius", function ()
-		Gladius.frame:SetBackdrop( {} );
-		Gladius.frame:EnableMouse( false );
+		hooksecurefunc( Gladius, "CreateFrame", me.CreateFrame );
+		hooksecurefunc( Gladius, "CreateButton", me.CreateButton );
 
 		-- Force glaze texture
 		Gladius.options.args.bars.args.colors.args.barTexture.values[ "_Clean" ] = "_Clean";
 		Gladius.db.profile.barTexture = "_Clean";
-
-		for UnitID, Button in pairs( Gladius.buttons ) do
-			-- Black backdrop
-			local Background = Button:CreateTexture( nil, "BACKGROUND" );
-			Background:SetTexture( 0, 0, 0 );
-			Background:SetPoint( "TOPRIGHT", Button.secure, 2, 2 );
-			Background:SetPoint( "BOTTOMLEFT", Button.secure, -2, -4 );
-
-			-- Left icon
-			Button.classIcon:SetVertexColor( 0.25, 0.25, 0.25 );
-			_Clean.RemoveButtonIconBorder( Button.auraFrame.icon );
-
-			-- Fade bar backgrounds
-			Button.health.bg:SetAlpha( 0.05 );
-			Button.mana.bg:SetAlpha( 0.1 );
-			Button.castBar.bg:SetAlpha( 0.05 );
-
-			-- Use "healer mode" reverse bars that blend colors
-			local Health = Button.health;
-			Health.Text = Button.healthText;
-			Health.Texture = Health:CreateTexture( nil, "ARTWORK" );
-			Health.Texture:SetPoint( "TOPRIGHT" );
-			Health.Texture:SetPoint( "BOTTOM" );
-			Health.Texture:SetTexture( Health:GetStatusBarTexture():GetTexture() );
-			Health:SetStatusBarTexture( nil );
-			Health.SetStatusBarTexture  = me.HealthSetStatusBarTexture;
-			_Clean.HookScript( Health, "OnValueChanged", me.HealthOnValueChanged );
-			_Clean.HookScript( Health, "OnSizeChanged", me.HealthOnSizeChanged );
-
-			-- Use class color for name
-			hooksecurefunc( Health, "SetStatusBarColor", me.HealthSetStatusBarColor );
-			Button.text.SetTextColorBackup = Button.text.SetTextColor;
-			Button.text.SetTextColor = _Clean.NilFunction;
-
-			-- Color mana text like mana bar
-			hooksecurefunc( Button.mana, "SetStatusBarColor", me.ManaSetStatusBarColor );
-			Button.manaText.SetTextColorBackup = Button.manaText.SetTextColor;
-			Button.manaText.SetTextColor = _Clean.NilFunction;
-
-			-- Remove original highlight and use mouseover highlight
-			local Secure = Button.secure; -- Actual unit button
-			Secure.Highlight = Secure:CreateTexture( nil, "OVERLAY" );
-			Secure.Highlight:Hide();
-			Secure.Highlight:SetAllPoints( Background );
-			Secure.Highlight:SetBlendMode( "ADD" );
-			Secure.Highlight:SetTexture( Button.highlight:GetTexture() );
-			Button.highlight:SetTexture( nil );
-			_Clean.HookScript( Secure, "OnEnter", me.OnEnter );
-			_Clean.HookScript( Secure, "OnLeave", me.OnLeave );
-
-			-- Remove ranks from casting text
-			local SpellText = Button.castBar.spellText;
-			SpellText.SetTextBackup = SpellText.SetText;
-			SpellText.SetText = me.SpellSetText;
-		end
 	end );
 end
