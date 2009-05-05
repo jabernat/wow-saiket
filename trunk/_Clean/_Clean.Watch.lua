79bd20c2afb9240b591a86cefaf367e0e6af948e
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2009-05-05 02:39:02 +0000

    _Clean: Watch frame headers are now only clickable when control is held, and shift clicking on them stops tracking.  Their right click dropdown menu has been removed.

--- trunk/_Clean/_Clean.Watch.lua
+++ trunk/_Clean/_Clean.Watch.lua
@@ -16,7 +16,57 @@ _Clean.Watch = me;
   * Description: Completely disables the collapse button when "hidden".        *
   ****************************************************************************]]
 function me:CollapseButtonEnable ()
-	self:EnableMouse( self:IsEnabled() == 1 )
+	self:EnableMouse( self:IsEnabled() == 1 );
+end
+
+
+--[[****************************************************************************
+  * Function: _Clean.Watch:WatchHeaderOnLeftClick                              *
+  * Description: Stops tracking if shift is held.                              *
+  ****************************************************************************]]
+do
+	local Backup = WatchFrameLinkButtonTemplate_OnLeftClick;
+	function me:WatchHeaderOnLeftClick ( ... )
+		if ( IsShiftKeyDown() ) then -- Stop tracking
+			CloseDropDownMenus();
+			if ( self.type == "QUEST" ) then
+				RemoveQuestWatch( self.index );
+				WatchFrame_Update();
+				if ( QuestLogFrame:IsVisible() ) then
+					QuestLog_Update();
+				end
+			elseif ( self.type == "ACHIEVEMENT" ) then
+				( AchievementButton_ToggleTracking or RemoveTrackedAchievement )( self.index );
+			end
+
+		else
+			return Backup( self, ... );
+		end
+	end
+end
+--[[****************************************************************************
+  * Function: _Clean.Watch.WatchHeaderHookFrame                                *
+  ****************************************************************************]]
+function me.WatchHeaderHookFrame ( Frame )
+	_Clean.AddLockedButton( Frame );
+	Frame:RegisterForClicks( "LeftButtonUp" );
+end
+--[[****************************************************************************
+  * Function: _Clean.Watch:WatchHeaderGetFrame                                 *
+  * Description: Hooks newly made header buttons.                              *
+  ****************************************************************************]]
+do
+	local Backup = WatchFrame.buttonCache.GetFrame;
+	function me:WatchHeaderGetFrame ( ... )
+		local NumFrames = self.numFrames;
+		local Frame = Backup( self, ... );
+
+		if ( NumFrames ~= self.numFrames ) then -- Created new frame
+			me.WatchHeaderHookFrame( Frame )
+		end
+
+		return Frame;
+	end
 end
 
 
@@ -29,4 +79,14 @@ end
 do
 	hooksecurefunc( WatchFrameCollapseExpandButton, "Enable", me.CollapseButtonEnable );
 	hooksecurefunc( WatchFrameCollapseExpandButton, "Disable", me.CollapseButtonEnable );
+
+	WatchFrameLinkButtonTemplate_OnLeftClick = me.WatchHeaderOnLeftClick;
+
+	WatchFrame.buttonCache.GetFrame = me.WatchHeaderGetFrame;
+	for _, Frame in ipairs( WatchFrame.buttonCache.frames ) do
+		me.WatchHeaderHookFrame( Frame );
+	end
+	for _, Frame in ipairs( WatchFrame.buttonCache.usedFrames ) do
+		me.WatchHeaderHookFrame( Frame );
+	end
 end
