14638aa00d3917daea3f1bead16419df8a6adbfa
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2009-06-15 01:15:50 +0000

    _NPCScan.Overlay: Initial commit of polygonal mob map overlays.

---
+++ trunk/_NPCScan.Overlay/Locales/Locale-enUS.lua
@@ -0,0 +1,10 @@
+--[[****************************************************************************
+  * _NPCScan.Overlay by Saiket                                                 *
+  * Locales/Locale-enUS.lua - Localized string constants (en-US).              *
+  ****************************************************************************]]
+
+
+do
+	_NPCScanLocalization.OVERLAY = setmetatable( {
+	}, getmetatable( _NPCScanLocalization ) );
+end
