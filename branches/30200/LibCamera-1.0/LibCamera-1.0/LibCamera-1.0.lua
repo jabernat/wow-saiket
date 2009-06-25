8298fb866bfd9578415055c066586412dcc7cb71
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2009-06-25 22:25:45 +0000

    LibCamera-1.0: Incremented minor LibStub version.

--- branches/30200/LibCamera-1.0/LibCamera-1.0/LibCamera-1.0.lua
+++ branches/30200/LibCamera-1.0/LibCamera-1.0/LibCamera-1.0.lua
@@ -4,7 +4,7 @@
   ****************************************************************************]]
 
 
-local MAJOR, MINOR = "LibCamera-1.0", 4;
+local MAJOR, MINOR = "LibCamera-1.0", 5;
 
 local lib = LibStub:NewLibrary( MAJOR, MINOR );
 if ( not lib ) then
