a29d7ca4c41d1a85c797a367ca31cb5c66ad2aa9
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2009-08-20 20:34:50 +0000

    _NPCScan: Tagged 3.2.0.5 release.

---
+++ tags/_NPCScan/3.2.0.5/_NPCScan.TamableIDs.lua
@@ -0,0 +1,96 @@
+--[[****************************************************************************
+  * _NPCScan by Saiket                                                         *
+  * _NPCScan.TamableIDs.lua - IDs of all tamable rare mobs.                    *
+  ****************************************************************************]]
+
+
+-- Extracted from WowHead filter results:
+-- http://www.wowhead.com/?npcs&filter=cl=4:2;fa=24:4:26:5:7:2:38:45:8:6:39:30:9:25:37:34:11:31:43:20:35:41:3:46:33:12:21:32:44:27:1:42
+_NPCScan.TamableIDs = {
+	[ 32481 ] = true; -- Aotona
+	[ 14222 ] = true; -- Araga
+	[ 5349 ] = true; -- Arash-ethis
+	[ 5834 ] = true; -- Azzere the Skyblade
+	[ 2753 ] = true; -- Barnabus
+	[ 14280 ] = true; -- Big Samras
+	[ 1130 ] = true; -- Bjarn
+	[ 4425 ] = true; -- Blind Hunter
+	[ 2850 ] = true; -- Broken Tooth
+	[ 616 ] = true; -- Chatter
+	[ 8301 ] = true; -- Clack the Reaver
+	[ 6582 ] = true; -- Clutchmother Zavas
+	[ 14223 ] = true; -- Cranky Benj
+	[ 14279 ] = true; -- Creepthess
+	[ 18241 ] = true; -- Crusty
+	[ 4380 ] = true; -- Darkmist Widow
+	[ 14232 ] = true; -- Dart
+	[ 5823 ] = true; -- Death Flayer
+	[ 14339 ] = true; -- Death Howl
+	[ 10077 ] = true; -- Deathmaw
+	[ 5865 ] = true; -- Dishu
+	[ 14430 ] = true; -- Duskstalker
+	[ 14228 ] = true; -- Giggler
+	[ 33776 ] = true; -- Gondria
+	[ 12431 ] = true; -- Gorefang
+	[ 17144 ] = true; -- Goretooth
+	[ 8207 ] = true; -- Greater Firebird
+	[ 14472 ] = true; -- Gretheer
+	[ 14477 ] = true; -- Grubthor
+	[ 8303 ] = true; -- Grunter
+	[ 8205 ] = true; -- Haarka the Ravenous
+	[ 14234 ] = true; -- Hayoc
+	[ 5828 ] = true; -- Humar the Pridelord
+	[ 8213 ] = true; -- Ironback
+	[ 32485 ] = true; -- King Krush
+	[ 6584 ] = true; -- King Mosh
+	[ 14476 ] = true; -- Krellack
+	[ 12433 ] = true; -- Krethis Shadowspinner
+	[ 14491 ] = true; -- Kurmokk
+	[ 14473 ] = true; -- Lapress
+	[ 2476 ] = true; -- Large Loch Crocolisk
+	[ 1112 ] = true; -- Leech Widow
+	[ 32517 ] = true; -- Loque'nahak
+	[ 14268 ] = true; -- Lord Condar
+	[ 521 ] = true; -- Lupos
+	[ 3068 ] = true; -- Mazzranache
+	[ 10644 ] = true; -- Mist Howler
+	[ 14344 ] = true; -- Mongress
+	[ 471 ] = true; -- Mother Fang
+	[ 8208 ] = true; -- Murderous Blisterpaw
+	[ 574 ] = true; -- Naraxis
+	[ 20932 ] = true; -- Nuramoc
+	[ 8211 ] = true; -- Old Cliff Jumper
+	[ 5352 ] = true; -- Old Grizzlegut
+	[ 12432 ] = true; -- Old Vicejaw
+	[ 14343 ] = true; -- Olm the Wise
+	[ 14237 ] = true; -- Oozeworm
+	[ 10200 ] = true; -- Rak'shiri
+	[ 8300 ] = true; -- Ravage
+	[ 6581 ] = true; -- Ravasaur Matriarch
+	[ 1140 ] = true; -- Razormaw Matriarch
+	[ 8277 ] = true; -- Rekk'tilac
+	[ 10357 ] = true; -- Ressan the Needler
+	[ 14233 ] = true; -- Ripscale
+	[ 3581 ] = true; -- Sewer Beast
+	[ 2175 ] = true; -- Shadowclaw
+	[ 14266 ] = true; -- Shanda the Spinner
+	[ 4132 ] = true; -- Silithid Ravager
+	[ 35189 ] = true; -- Skoll
+	[ 5356 ] = true; -- Snarler
+	[ 5829 ] = true; -- Snort the Heckler
+	[ 8204 ] = true; -- Soriid the Devourer
+	[ 8299 ] = true; -- Spiteflayer
+	[ 10359 ] = true; -- Sri'skulk
+	[ 2172 ] = true; -- Strider Clutchmother
+	[ 5842 ] = true; -- Takk the Leaper
+	[ 32475 ] = true; -- Terror Spinner
+	[ 8660 ] = true; -- The Evalcharr
+	[ 5807 ] = true; -- The Rake
+	[ 11497 ] = true; -- The Razza
+	[ 1132 ] = true; -- Timber
+	[ 6585 ] = true; -- Uhk'loc
+	[ 12037 ] = true; -- Ursol'lok
+	[ 5937 ] = true; -- Vile Sting
+	[ 462 ] = true; -- Vultros
+	[ 2931 ] = true; -- Zaricotl
+};
