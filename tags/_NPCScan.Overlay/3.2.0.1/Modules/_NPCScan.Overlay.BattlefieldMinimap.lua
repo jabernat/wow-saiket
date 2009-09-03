7d0c62d6ce6863f8ae6ccb7781e1cfc047ffb37e
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2009-09-03 04:06:03 +0000

    _NPCScan.Overlay: Tagged 3.2.0.1 release.

---
+++ tags/_NPCScan.Overlay/3.2.0.1/Modules/_NPCScan.Overlay.BattlefieldMinimap.lua
@@ -0,0 +1,63 @@
+--[[****************************************************************************
+  * _NPCScan.Overlay by Saiket                                                 *
+  * _NPCScan.Overlay.BattlefieldMinimap.lua - Canvas for the                   *
+  *   Blizzard_BattlefieldMinimap addon.                                       *
+  ****************************************************************************]]
+
+
+local Overlay = _NPCScan.Overlay;
+local me = CreateFrame( "Frame" );
+Overlay.BattlefieldMinimap = me;
+
+me.Label = _NPCScanLocalization.OVERLAY.MODULE_BATTLEFIELDMINIMAP;
+me.AlphaDefault = 0.8;
+
+
+
+
+--[[****************************************************************************
+  * Function: _NPCScan.Overlay.BattlefieldMinimap:Repaint                      *
+  ****************************************************************************]]
+do
+	local function PaintPath ( PathData, R, G, B )
+		Overlay.PathAdd( me, PathData, "OVERLAY", R, G, B );
+	end
+	function me:Repaint ( Map )
+		Overlay.ApplyZone( Map, PaintPath );
+	end
+end
+
+
+--[[****************************************************************************
+  * Function: _NPCScan.Overlay.BattlefieldMinimap:OnLoad                       *
+  ****************************************************************************]]
+function me:OnLoad ()
+	me:SetParent( BattlefieldMinimap );
+
+	-- Inherit standard map update code from WorldMap module
+	local WorldMap = Overlay.WorldMap;
+	me.Enable = WorldMap.Enable;
+	me.Disable = WorldMap.Disable;
+	me.Update = WorldMap.Update;
+	WorldMap.OnLoad( me );
+
+	me:ClearAllPoints();
+	me:SetPoint( "TOPLEFT" );
+	local Tile = _G[ "BattlefieldMinimap"..NUM_WORLDMAP_DETAIL_TILES ];
+	me:SetPoint( "BOTTOMRIGHT", Tile, -22 / 256 * Tile:GetWidth(), 100 / 256 * Tile:GetHeight() );
+
+	if ( Overlay.Options.Modules[ "BattlefieldMinimap" ] == true ) then
+		me:Enable();
+	end
+end
+
+
+
+
+--------------------------------------------------------------------------------
+-- Function Hooks / Execution
+-----------------------------
+
+do
+	Overlay.ModuleRegister( "BattlefieldMinimap", me, "Blizzard_BattlefieldMinimap" );
+end
