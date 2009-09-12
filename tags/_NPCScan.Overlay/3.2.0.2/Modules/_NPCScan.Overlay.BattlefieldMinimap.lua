a54d0739d6df3b32ef54ae592dc64bfae7586786
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2009-09-12 07:56:38 +0000

    _NPCScan.Overlay: Tagged 3.2.0.2 release.

---
+++ tags/_NPCScan.Overlay/3.2.0.2/Modules/_NPCScan.Overlay.BattlefieldMinimap.lua
@@ -0,0 +1,63 @@
+--[[****************************************************************************
+  * _NPCScan.Overlay by Saiket                                                 *
+  * _NPCScan.Overlay.BattlefieldMinimap.lua - Canvas for the                   *
+  *   Blizzard_BattlefieldMinimap addon.                                       *
+  ****************************************************************************]]
+
+
+local Overlay = _NPCScan.Overlay;
+local me = CreateFrame( "Frame" );
+Overlay.BattlefieldMinimap = me;
+
+me.Label = _NPCScanLocalization.OVERLAY.MODULE_BATTLEFIELDMINIMAP;
+me.AlphaDefault = 0.8;
+
+
+
+
+--[[****************************************************************************
+  * Function: _NPCScan.Overlay.BattlefieldMinimap:Repaint                      *
+  ****************************************************************************]]
+do
+	local function PaintPath ( PathData, FoundX, FoundY, R, G, B, NpcID )
+		Overlay.DrawPath( me, PathData, "ARTWORK", R, G, B );
+		if ( FoundX ) then
+			Overlay.DrawFound( me, FoundX, FoundY, Overlay.DetectionRadius / Overlay.GetZoneSize( Overlay.NPCMaps[ NpcID ] ), "OVERLAY", R, G, B );
+		end
+	end
+	function me:Repaint ( Map )
+		Overlay.ApplyZone( Map, PaintPath );
+	end
+end
+
+
+--[[****************************************************************************
+  * Function: _NPCScan.Overlay.BattlefieldMinimap:OnLoad                       *
+  ****************************************************************************]]
+function me:OnLoad ()
+	me:SetParent( BattlefieldMinimap );
+	me:SetAllPoints();
+
+	-- Inherit standard map update code from WorldMap module
+	local WorldMap = Overlay.WorldMap;
+	me.Enable = WorldMap.Enable;
+	me.Disable = WorldMap.Disable;
+	me.Update = WorldMap.Update;
+	WorldMap.OnLoad( me );
+
+	if ( Overlay.Options.Modules[ "BattlefieldMinimap" ] == true ) then
+		me:Enable();
+		me:Update();
+	end
+end
+
+
+
+
+--------------------------------------------------------------------------------
+-- Function Hooks / Execution
+-----------------------------
+
+do
+	Overlay.ModuleRegister( "BattlefieldMinimap", me, "Blizzard_BattlefieldMinimap" );
+end
