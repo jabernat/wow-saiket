7d130b3ea3120e50229a7306d93570ce541ec531
Saiket <saiket.wow@gmail.com>
2009-12-08 10:56:05 +0000

    _Clean.HUD: Tagged 3.3.0.1 release.

---
+++ 3.3.0.1/_Clean.HUD/_Clean.HUD.Flags.lua
@@ -0,0 +1,67 @@
+--[[****************************************************************************
+  * _Clean.HUD by Saiket                                                       *
+  * _Clean.HUD.Flags.lua - Text AFK/DND indicator for the player.              *
+  ****************************************************************************]]
+
+
+local L = _CleanLocalization.HUD;
+local _Clean = _Clean;
+local me = CreateFrame( "Frame", nil, UIParent );
+_Clean.HUD.Flags = me;
+
+me.Text = me:CreateFontString( nil, "ARTWORK", "GameFontNormalHuge" );
+
+
+
+
+--[[****************************************************************************
+  * Function: _Clean.HUD.Flags:PLAYER_FLAGS_CHANGED                            *
+  ****************************************************************************]]
+function me:PLAYER_FLAGS_CHANGED ( _, UnitID )
+	if ( UnitID == "player" ) then
+		local Status;
+		if ( UnitIsAFK( "player" ) ) then
+			Status = L.FLAG_AFK;
+		elseif ( UnitIsDND( "player" ) ) then
+			Status = L.FLAG_DND;
+		end
+		if ( Status ) then
+			self.Text:SetText( Status );
+			self:Show();
+		else
+			self:Hide();
+		end
+	end
+end
+--[[****************************************************************************
+  * Function: _Clean.HUD.Flags:PLAYER_ENTERING_WORLD                           *
+  ****************************************************************************]]
+function me:PLAYER_ENTERING_WORLD ()
+	self:Hide();
+end
+--[[****************************************************************************
+  * Function: _Clean.HUD.Flags:OnUpdate                                        *
+  ****************************************************************************]]
+function me:OnUpdate ()
+	me:SetScript( "OnUpdate", nil );
+	me.OnUpdate = nil;
+
+	self:PLAYER_FLAGS_CHANGED( nil, "player" );
+	self:RegisterEvent( "PLAYER_ENTERING_WORLD" );
+end
+
+
+
+
+--------------------------------------------------------------------------------
+-- Function Hooks / Execution
+-----------------------------
+
+do
+	me:SetScript( "OnUpdate", me.OnUpdate );
+	me:SetScript( "OnEvent", _Clean.OnEvent );
+	me:RegisterEvent( "PLAYER_FLAGS_CHANGED" );
+
+	me:SetFrameStrata( "BACKGROUND" );
+	me.Text:SetPoint( "BOTTOM", AutoFollowStatusText, "TOP" );
+end
