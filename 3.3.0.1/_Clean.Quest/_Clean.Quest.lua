3c9000fe1d221a82a09f6aceb96154deaee54bdf
Saiket <saiket.wow@gmail.com>
2009-12-08 10:57:36 +0000

    _Clean.Quest: Tagged 3.3.0.1 release.

---
+++ 3.3.0.1/_Clean.Quest/_Clean.Quest.lua
@@ -0,0 +1,73 @@
+--[[****************************************************************************
+  * _Clean.Quest by Saiket                                                     *
+  * _Clean.Quest.lua - Modifies the quest log.                                 *
+  ****************************************************************************]]
+
+
+if ( IsAddOnLoaded( "Carbonite" ) ) then
+	return;
+end
+local L = _CleanLocalization.Quest;
+local _Clean = _Clean;
+local me = {};
+_Clean.Quest = me;
+
+
+
+
+--[[****************************************************************************
+  * Function: _Clean.Quest.QuestLogUpdate                                      *
+  * Description: Add level information and quest type data to the titles of    *
+  *   quests for when they're linked, and remove the default quest type text.  *
+  ****************************************************************************]]
+do
+	local GetQuestLogTitle = GetQuestLogTitle;
+	function me.QuestLogUpdate ()
+		if ( not QuestLogFrame:IsShown() ) then
+			return;
+		end
+
+		for _, Button in ipairs( QuestLogScrollFrame.buttons ) do
+			if ( not Button:IsShown() ) then
+				break;
+			end
+
+			if ( not Button.isHeader ) then
+				local Index = Button:GetID();
+
+				local Title, Level, Tag, _, _, _, Completed, IsDaily = GetQuestLogTitle( Index );
+				if ( Tag ) then
+					Tag = L.Types[ Tag:match( L.DAILY_PATTERN ) or Tag ];
+				end
+				if ( IsDaily ) then
+					Tag = L.DAILY_FORMAT:format( Tag or "" );
+				end
+				Button.normalText:SetFormattedText( L.TITLE_FORMAT, Level, Tag or "", Title );
+				Button.tag:SetText( L.Completed[ Completed ] );
+			end
+		end
+	end
+end
+
+
+
+
+--------------------------------------------------------------------------------
+-- Function Hooks / Execution
+-----------------------------
+
+do
+	-- Make titles expand when no tag is present
+	for Index, Button in ipairs( QuestLogScrollFrame.buttons ) do
+		Button.tag:SetWidth( 0 ); -- Cause width to scale to text contents
+		Button.normalText:SetPoint( "RIGHT", Button.tag, "LEFT" );
+		Button.groupMates:ClearAllPoints();
+		Button.groupMates:SetPoint( "RIGHT", Button.normalText, "LEFT" );
+		Button.check:ClearAllPoints();
+		Button.check:SetPoint( "RIGHT", Button.normalText, "LEFT" );
+	end
+	QuestLogTitleButton_Resize = _Clean.NilFunction;
+
+	hooksecurefunc( "QuestLog_Update", me.QuestLogUpdate );
+	QuestLogScrollFrame.update = QuestLog_Update;
+end
