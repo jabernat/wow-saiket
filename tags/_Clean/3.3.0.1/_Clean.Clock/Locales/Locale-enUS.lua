d7986f08db6d96f8abb3d7fed46d5753ab3f2e6c
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2009-12-08 10:54:43 +0000

    _Clean.Clock: Tagged 3.3.0.1 release.

---
+++ tags/_Clean/3.3.0.1/_Clean.Clock/Locales/Locale-enUS.lua
@@ -0,0 +1,11 @@
+--[[****************************************************************************
+  * _Clean.Clock by Saiket                                                     *
+  * Locales/Locale-enUS.lua - Localized string constants (en-US).              *
+  ****************************************************************************]]
+
+
+do
+	_CleanLocalization.Clock = setmetatable( {
+		TIME_FORMAT = "T%02d:%02d:%02d";
+	}, getmetatable( _CleanLocalization ) );
+end
