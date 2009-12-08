7859d04a8dc88e69aadeb858f3bf45f806e7e6ca
Saiket <saiket.wow@gmail.com>
2009-12-08 10:53:15 +0000

    _Clean:CastingBar: Tagged 3.3.0.1 release.

---
+++ 3.3.0.1/_Clean.CastingBar/Locales/Locale-enUS.lua
@@ -0,0 +1,11 @@
+--[[****************************************************************************
+  * _Clean.CastingBar by Saiket                                                *
+  * Locales/Locale-enUS.lua - Localized string constants (en-US).              *
+  ****************************************************************************]]
+
+
+do
+	_CleanLocalization.CastingBar = setmetatable( {
+		TIME_FORMAT = "(%.1fs)"; -- Seconds
+	}, getmetatable( _CleanLocalization ) );
+end
