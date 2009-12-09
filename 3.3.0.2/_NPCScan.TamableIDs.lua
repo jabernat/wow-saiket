9f67f69c94cd0bf5f823ab73d6bc66c2fa8602ae
Saiket <saiket.wow@gmail.com>
2009-12-09 00:27:33 +0000

    _NPCScan: Tagged 3.3.0.2 release.

---
+++ 3.3.0.2/_NPCScan.TamableIDs.lua
@@ -0,0 +1,97 @@
+--[[****************************************************************************
+  * _NPCScan by Saiket                                                         *
+  * _NPCScan.TamableIDs.lua - IDs of all tamable rare mobs.                    *
+  ****************************************************************************]]
+
+
+-- Extracted from WowHead filter results:
+-- http://www.wowhead.com/?npcs&filter=cl=4:2;fa=24:4:26:5:7:2:38:45:8:6:39:30:9:25:37:34:11:31:43:20:35:41:3:46:33:12:21:32:44:27:1:42
+_NPCScan.TamableIDs = { -- [ NpcID ] = ZoneID; (See WorldMapArea.dbc)
+	[ 32481 ] = 493; -- Aotona
+	[ 14222 ] = 15; -- Araga
+	[ 5349 ] = 121; -- Arash-ethis
+	[ 38453 ] = 490; -- Arcturis
+	[ 5834 ] = 11; -- Azzere the Skyblade
+	[ 2753 ] = 17; -- Barnabus
+	[ 14280 ] = 24; -- Big Samras
+	[ 1130 ] = 27; -- Bjarn
+	[ 4425 ] = true; -- Blind Hunter
+	[ 2850 ] = 17; -- Broken Tooth
+	[ 616 ] = 36; -- Chatter
+	[ 8301 ] = 19; -- Clack the Reaver
+	[ 6582 ] = 201; -- Clutchmother Zavas
+	[ 14223 ] = 15; -- Cranky Benj
+	[ 14279 ] = 24; -- Creepthess
+	[ 18241 ] = 101; -- Crusty
+	[ 4380 ] = 141; -- Darkmist Widow
+	[ 14232 ] = 141; -- Dart
+	[ 5823 ] = 4; -- Death Flayer
+	[ 14339 ] = 182; -- Death Howl
+	[ 10077 ] = 29; -- Deathmaw
+	[ 5865 ] = 11; -- Dishu
+	[ 14430 ] = 41; -- Duskstalker
+	[ 14228 ] = 101; -- Giggler
+	[ 33776 ] = 496; -- Gondria
+	[ 12431 ] = 21; -- Gorefang
+	[ 17144 ] = 477; -- Goretooth
+	[ 8207 ] = 161; -- Greater Firebird
+	[ 14472 ] = 261; -- Gretheer
+	[ 14477 ] = 261; -- Grubthor
+	[ 8303 ] = 19; -- Grunter
+	[ 8205 ] = 161; -- Haarka the Ravenous
+	[ 14234 ] = 141; -- Hayoc
+	[ 5828 ] = 11; -- Humar the Pridelord
+	[ 8213 ] = 26; -- Ironback
+	[ 32485 ] = 493; -- King Krush
+	[ 6584 ] = 201; -- King Mosh
+	[ 14476 ] = 261; -- Krellack
+	[ 12433 ] = 21; -- Krethis Shadowspinner
+	[ 14491 ] = 37; -- Kurmokk
+	[ 14473 ] = 261; -- Lapress
+	[ 2476 ] = 35; -- Large Loch Crocolisk
+	[ 1112 ] = 40; -- Leech Widow
+	[ 32517 ] = 493; -- Loque'nahak
+	[ 14268 ] = 35; -- Lord Condar
+	[ 521 ] = 34; -- Lupos
+	[ 3068 ] = 9; -- Mazzranache
+	[ 10644 ] = 43; -- Mist Howler
+	[ 14344 ] = 182; -- Mongress
+	[ 471 ] = 30; -- Mother Fang
+	[ 8208 ] = 161; -- Murderous Blisterpaw
+	[ 574 ] = 34; -- Naraxis
+	[ 20932 ] = 479; -- Nuramoc
+	[ 8211 ] = 26; -- Old Cliff Jumper
+	[ 5352 ] = 121; -- Old Grizzlegut
+	[ 12432 ] = 21; -- Old Vicejaw
+	[ 14343 ] = 182; -- Olm the Wise
+	[ 14237 ] = 141; -- Oozeworm
+	[ 10200 ] = 281; -- Rak'shiri
+	[ 8300 ] = 19; -- Ravage
+	[ 6581 ] = 201; -- Ravasaur Matriarch
+	[ 1140 ] = 40; -- Razormaw Matriarch
+	[ 8277 ] = 28; -- Rekk'tilac
+	[ 10357 ] = 20; -- Ressan the Needler
+	[ 14233 ] = 141; -- Ripscale
+	[ 3581 ] = 301; -- Sewer Beast
+	[ 2175 ] = 42; -- Shadowclaw
+	[ 14266 ] = 35; -- Shanda the Spinner
+	[ 4132 ] = 61; -- Silithid Ravager
+	[ 35189 ] = 495; -- Skoll
+	[ 5356 ] = 121; -- Snarler
+	[ 5829 ] = 11; -- Snort the Heckler
+	[ 8204 ] = 161; -- Soriid the Devourer
+	[ 8299 ] = 19; -- Spiteflayer
+	[ 10359 ] = 20; -- Sri'skulk
+	[ 2172 ] = 42; -- Strider Clutchmother
+	[ 5842 ] = 11; -- Takk the Leaper
+	[ 32475 ] = 496; -- Terror Spinner
+	[ 8660 ] = 181; -- The Evalcharr
+	[ 5807 ] = 9; -- The Rake
+	[ 11497 ] = 13; -- The Razza (Dire Maul courtyard, zoneless)
+	[ 1132 ] = 27; -- Timber
+	[ 6585 ] = 201; -- Uhk'loc
+	[ 12037 ] = 43; -- Ursol'lok
+	[ 5937 ] = 61; -- Vile Sting
+	[ 462 ] = 39; -- Vultros
+	[ 2931 ] = 17; -- Zaricotl
+};
