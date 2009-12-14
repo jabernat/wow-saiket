da6f9d0e8a2dc527136ee11edbd9113d9b5b8211
Saiket <saiket.wow@gmail.com>
2009-12-14 22:10:08 +0000

    _Clean.Tooltip: Reworked tooltip icons to work in all cases instead of just for hyperlinks.  Also added class icons to the GameTooltip.

--- _Clean.Tooltip/_Clean.Tooltip.lua
+++ _Clean.Tooltip/_Clean.Tooltip.lua
@@ -4,7 +4,6 @@
   ****************************************************************************]]
 
 
--- NOTE(Add class icons to the main gametooltip, and then skin tooltip frames.)
 local L = _CleanLocalization.Tooltip;
 local _Clean = _Clean;
 local me = {};
@@ -28,87 +27,137 @@ end
 
 
 --[[****************************************************************************
-  * Function: _Clean.Tooltip.UpdateUnitGuild                                   *
-  * Description: Add guild names to the right of the character's name.         *
+  * Function: _Clean.Tooltip:IconCreate                                        *
+  * Description: Creates an icon on the tooltip frame.                         *
   ****************************************************************************]]
-function me:UpdateUnitGuild ( UnitID )
-	if ( UnitExists( UnitID ) and UnitIsPlayer( UnitID ) ) then
-		local GuildName = GetGuildInfo( UnitID );
-		if ( GuildName ) then
-			local Text = _G[ self:GetName().."TextLeft2" ];
-			Text:SetFormattedText( L.GUILD_FORMAT, GuildName );
-			Text:SetTextColor( GRAY_FONT_COLOR.r, GRAY_FONT_COLOR.g, GRAY_FONT_COLOR.b );
-			self:AppendText( "" ); -- Automatically resize
+do
+	local function OnTooltipCleared ( self )
+		self.Icon:Hide();
+		self.Normal:Hide();
+	end
+	function me:IconCreate ()
+		if ( not self.Icon ) then
+			local Icon, Normal = self:CreateTexture( nil, "ARTWORK" ), self:CreateTexture( nil, "OVERLAY" );
+			self.Icon, self.Normal = Icon, Normal;
+			Icon:SetWidth( IconSize );
+			Icon:SetHeight( IconSize );
+			Icon:SetPoint( "TOPRIGHT", self, "TOPLEFT", 2, -2 );
+			_Clean.SkinButton( nil, Icon, Normal );
+
+			self:HookScript( "OnTooltipCleared", OnTooltipCleared );
+			OnTooltipCleared( self );
 		end
 	end
 end
-
 --[[****************************************************************************
-  * Function: _Clean.Tooltip:OnTooltipSetUnit                                  *
-  * Description: Hooks a tooltip to update its contents when set.              *
+  * Function: _Clean.Tooltip:IconSet                                           *
+  * Description: Sets or hides the tooltip's icon.                             *
   ****************************************************************************]]
-function me:OnTooltipSetUnit ()
-	local UnitID = select( 2, self:GetUnit() );
-	if ( UnitID ) then
-		me.UpdateUnitGuild( self, UnitID );
+function me:IconSet ( Texture, ... )
+	local Icon, Normal = self.Icon, self.Normal;
+	Icon:SetTexture( Texture );
+	if ( Texture ) then
+		if ( ... ) then
+			Icon:SetTexCoord( ... );
+		else -- Restore default
+			_Clean.SkinButtonIcon( Icon );
+		end
+		Icon:Show();
+		Normal:Show();
+	else
+		Icon:Hide();
+		Normal:Hide();
 	end
 end
---[[****************************************************************************
-  * Function: _Clean.Tooltip:SetUnit                                           *
-  * Description: Updates the tooltip's guild info if the unit isn't IDed.      *
-  ****************************************************************************]]
-function me:SetUnit ( ActualUnitID )
-	local UnitID = select( 2, self:GetUnit() );
-	if ( not UnitID ) then -- OnTooltipSetUnit can't handle this case
-		me.UpdateUnitGuild( self, ActualUnitID );
-	end
-end
 
 
 --[[****************************************************************************
-  * Function: _Clean.Tooltip:RegisterGuild                                     *
+  * Function: _Clean.Tooltip:UnitRegister                                      *
+  * Description: Add a class icon and guild name brackets to unit tooltips.    *
   ****************************************************************************]]
-function me:RegisterGuild ()
-	self:HookScript( "OnTooltipSetUnit", me.OnTooltipSetUnit );
-	hooksecurefunc( self, "SetUnit", me.SetUnit );
-end
+do
+	local function Update ( self, UnitID )
+		if ( UnitExists( UnitID ) and UnitIsPlayer( UnitID ) ) then
+			local GuildName = GetGuildInfo( UnitID );
+			if ( GuildName ) then
+				local Text = _G[ self:GetName().."TextLeft2" ];
+				Text:SetFormattedText( L.GUILD_FORMAT, GuildName );
+				Text:SetTextColor( GRAY_FONT_COLOR.r, GRAY_FONT_COLOR.g, GRAY_FONT_COLOR.b );
+				self:AppendText( "" ); -- Automatically resize
+			end
 
-
-
-
---[[****************************************************************************
-  * Function: _Clean.Tooltip:SetHyperlink                                      *
-  * Description: Updates the new item icon texture to match the tooltip.       *
-  ****************************************************************************]]
-function me:SetHyperlink ( Link )
-	local Texture = nil;
-	if ( self:IsShown() ) then
-		if ( Link:match( "^item:" ) ) then
-			Texture = GetItemIcon( Link );
+			me.IconSet( self,
+				[[Interface\Glues\CharacterCreate\UI-CharacterCreate-Classes]],
+				unpack( CLASS_ICON_TCOORDS[ select( 2, UnitClass( UnitID ) ) ] ) );
 		else
-			local ID = Link:match( "^spell:(%d+)" );
+			me.IconSet( self );
+		end
+	end
+	local function OnTooltipSetUnit ( self )
+		local UnitID = select( 2, self:GetUnit() );
+		if ( UnitID ) then
+			Update( self, UnitID );
+		end
+	end
+	local function SetUnit ( self, ActualUnitID )
+		local UnitID = select( 2, self:GetUnit() );
+		if ( not UnitID and self:IsShown() ) then -- OnTooltipSetUnit can't handle this case
+			Update( self, ActualUnitID );
+		end
+	end
+	function me:UnitRegister ()
+		me.IconCreate( self );
+
+		hooksecurefunc( self, "SetUnit", SetUnit );
+		self:HookScript( "OnTooltipSetUnit", OnTooltipSetUnit );
+	end
+end
+--[[****************************************************************************
+  * Function: _Clean.Tooltip:ItemRegister                                      *
+  * Description: Add item icons to tooltips.                                   *
+  ****************************************************************************]]
+do
+	local function OnTooltipSetItem ( self )
+		me.IconSet( self, GetItemIcon( select( 2, self:GetItem() ) ) );
+	end
+	function me:ItemRegister ()
+		me.IconCreate( self );
+
+		self:HookScript( "OnTooltipSetItem", OnTooltipSetItem );
+	end
+end
+--[[****************************************************************************
+  * Function: _Clean.Tooltip:SpellRegister                                     *
+  * Description: Add spell icons to tooltips.                                  *
+  ****************************************************************************]]
+do
+	local function OnTooltipSetSpell ( self )
+		me.IconSet( self, ( select( 3, GetSpellInfo( select( 3, self:GetSpell() ) ) ) ) );
+	end
+	function me:SpellRegister ()
+		me.IconCreate( self );
+
+		self:HookScript( "OnTooltipSetSpell", OnTooltipSetSpell );
+	end
+end
+--[[****************************************************************************
+  * Function: _Clean.Tooltip:AchievementRegister                               *
+  * Description: Add achievement icons to tooltips.                            *
+  ****************************************************************************]]
+do
+	local function SetHyperlink ( self, Link )
+		if ( self:IsShown() ) then
+			local ID = Link:match( "^achievement:(%d+)" ) or Link:match( "|Hachievement:(%d+)" );
 			if ( ID ) then
-				Texture = select( 3, GetSpellInfo( ID ) );
-			else
-				ID = Link:match( "^achievement:(%d+)" );
-				if ( ID ) then
-					Texture = select( 10, GetAchievementInfo( ID ) );
-				end
+				me.IconSet( self, ( select( 10, GetAchievementInfo( ID ) ) ) );
 			end
 		end
 	end
-	self.Icon:SetTexture( Texture );
-end
---[[****************************************************************************
-  * Function: _Clean.Tooltip:RegisterIcon                                      *
-  ****************************************************************************]]
-function me:RegisterIcon ()
-	self.Icon = self:CreateTexture( nil, "ARTWORK" );
-	self.Icon:SetWidth( IconSize );
-	self.Icon:SetHeight( IconSize );
-	self.Icon:SetPoint( "TOPRIGHT", self, "TOPLEFT", 2, -2 );
+	function me:AchievementRegister ()
+		me.IconCreate( self );
 
-	hooksecurefunc( self, "SetHyperlink", me.SetHyperlink );
+		hooksecurefunc( self, "SetHyperlink", SetHyperlink );
+	end
 end
 
 
@@ -121,7 +170,10 @@ end
 do
 	hooksecurefunc( "GameTooltip_SetDefaultAnchor", me.SetDefaultAnchor );
 
-	me.RegisterGuild( GameTooltip );
-	me.RegisterGuild( ItemRefTooltip );
-	me.RegisterIcon( ItemRefTooltip );
+	me.UnitRegister( GameTooltip );
+
+	me.UnitRegister( ItemRefTooltip );
+	me.ItemRegister( ItemRefTooltip );
+	me.SpellRegister( ItemRefTooltip );
+	me.AchievementRegister( ItemRefTooltip );
 end
