a5ecc626db9e4b18ea7bad3a6abeeb0294c94229
Saiket <saiket.wow@gmail.com>
2010-01-23 07:11:42 +0000

    _Underscore.WorldMap: Party/raid member blips are now class colored on the world map (and Battlefield Minimap).

--- _Underscore.WorldMap/_Underscore.WorldMap.lua
+++ _Underscore.WorldMap/_Underscore.WorldMap.lua
@@ -21,6 +21,43 @@ me.OffsetX, me.OffsetY = -8, -16;
 
 
 --[[****************************************************************************
+  * Function: _Underscore.WorldMap.UpdateUnit                                  *
+  * Description: Adds class color to player blips on the map.                  *
+  ****************************************************************************]]
+do
+	local Colors = _Underscore.Colors.class;
+	local InactiveColor = { 0.5, 0.2, 0.8 };
+	local Top, Bottom, Left, Right = 0.25, 0.5, 7 / 8, 1; -- Row 2, column 8: White template blip
+	function me:UpdateUnit ()
+		local UnitID = self.unit or self.name;
+		local Icon = self.icon;
+
+		if ( UnitID and UnitIsPlayer( UnitID ) ) then -- Class colored icon
+			local Color;
+			if ( PlayerIsPVPInactive( UnitID ) ) then
+				Color = InactiveColor;
+			else
+				local _, Class = UnitClass( UnitID );
+				Color = Colors[ Class ];
+			end
+
+			Icon:SetTexture( [[Interface\Minimap\PartyRaidBlips]] );
+			Icon:SetVertexColor( unpack( Color ) );
+			if ( UnitPlayerOrPetInParty( UnitID ) ) then
+				Icon:SetTexCoord( Left, Right, Top, Bottom );
+			else -- Bottom half contains raid member blips
+				Icon:SetTexCoord( Left, Right, Top + 0.5, Bottom + 0.5 );
+			end
+		else -- Generic blip icon
+			Icon:SetTexture( [[Interface\WorldMap\WorldMapPartyIcon]] );
+			Icon:SetVertexColor( 1, 1, 1 );
+			Icon:SetTexCoord( 0, 1, 0, 1 );
+		end
+	end
+end
+
+
+--[[****************************************************************************
   * Function: _Underscore.WorldMap.DisableBlackout                             *
   * Description: Keeps the black background behind the map hidden.             *
   ****************************************************************************]]
@@ -80,6 +117,8 @@ do
 	me.Text:SetAllPoints();
 
 	WorldMapButton:HookScript( "OnUpdate", me.OnUpdate );
+
+	WorldMapUnit_Update = me.UpdateUnit;
 
 
 	hooksecurefunc( "WorldMap_ToggleSizeUp", me.DisableBlackout );
