834a420ed33a6580653f05e8c536bddb8add2efb
Saiket <saiket.wow@gmail.com>
2010-02-24 03:59:28 +0000

    _NPCScan.Overlay: The WorldMap toggle button doesn't overlap Mapster's stuff when sized down.

--- _NPCScan.Overlay/Modules/_NPCScan.Overlay.Mapster.lua
+++ _NPCScan.Overlay/Modules/_NPCScan.Overlay.Mapster.lua
@@ -9,10 +9,25 @@
 -----------------------------
 
 if ( IsAddOnLoaded( "Mapster" ) ) then
-	hooksecurefunc( LibStub( "AceAddon-3.0" ):GetAddon( "Mapster" ), "SetupMapButton", function ()
-		local Toggle = _NPCScan.Overlay.WorldMap.Toggle;
+	local Toggle = _NPCScan.Overlay.WorldMap.Toggle;
+	local Mapster = LibStub( "AceAddon-3.0" ):GetAddon( "Mapster" );
 
+	local function UpdateTogglePosition () -- Moves the checkbox so it doesn't overlap Mapster's stuff
 		Toggle:ClearAllPoints();
-		Toggle:SetPoint( "LEFT", MapsterOptionsButton, "RIGHT", 8, 0 );
+		if ( WorldMapFrame.sizedDown ) then -- Bottom right corner of map
+			local Label = _G[ Toggle:GetName().."Text" ];
+			Toggle:SetPoint( "BOTTOM", MapsterOptionsButton, 0, -2 );
+			Toggle:SetPoint( "RIGHT", WorldMapDetailFrame, -Label:GetStringWidth() - 12, 0 );
+		else -- Left side, near Mapster options button
+			Toggle:SetPoint( "LEFT", MapsterOptionsButton, "RIGHT", 8, 0 );
+		end
+	end
+
+	-- Hook size up/down commands once Mapster creates its options button
+	hooksecurefunc( Mapster, "OnEnable", function ()
+		-- Options button created
+		hooksecurefunc( Mapster, "SizeUp", UpdateTogglePosition );
+		hooksecurefunc( Mapster, "SizeDown", UpdateTogglePosition );
+		UpdateTogglePosition();
 	end );
 end
