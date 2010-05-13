4aae97b2d67eb8d8eb61f69eaecf64a636198883
Saiket <saiket.wow@gmail.com>
2010-05-13 19:36:37 +0000

    _NPCScan: Removed hooks to control RareSpawnOverlay.  The implementation was never quite right, and since a recent patch its own toggle checkboxes to turn paths back on are inaccessible.

--- _NPCScan/_NPCScan.Overlays.lua
+++ _NPCScan/_NPCScan.Overlays.lua
@@ -5,12 +5,8 @@
 
 
 local AddOnName = ...;
-local L = _NPCScanLocalization;
-local _NPCScan = _NPCScan;
 local me = {};
 _NPCScan.Overlays = me;
-
-me.Debug = false; -- Set to true so incompatible hooks will throw real errors
 
 local MESSAGE_REGISTER = "NpcOverlay_RegisterScanner";
 local MESSAGE_ADD = "NpcOverlay_Add";
@@ -61,44 +57,4 @@ end
 
 do
 	LibStub( "AceEvent-3.0" ):Embed( me );
-
-	-- Add message support for RareSpawnOverlay
-	if ( select( 4, GetAddOnInfo( "RareSpawnOverlay" ) ) ) then -- Enabled
-		local function SafeCall ( Func, ... )
-			if ( me.Debug ) then
-				Func( ... );
-			elseif ( not pcall( Func, ... ) ) then -- Error
-				me:UnregisterAllMessages();
-				_NPCScan.Print( L.OVERLAY_INCOMPATIBLE:format( "RareSpawnOverlay",
-					tostring( GetAddOnMetadata( "RareSpawnOverlay", "Version" ) ),
-					_NPCScan.Version ), RED_FONT_COLOR );
-			end
-		end
-
-		me:RegisterMessage( MESSAGE_REGISTER, function ( Event, AddOn )
-			if ( AddOn == AddOnName ) then -- _NPCScan registered
-				me:UnregisterMessage( Event );
-
-				local RSO, Show, Hide;
-				me:RegisterMessage( MESSAGE_ADD, function ( _, ID )
-					SafeCall( Show, RSO, ID );
-				end );
-				me:RegisterMessage( MESSAGE_REMOVE, function ( _, ID )
-					SafeCall( Hide, RSO, ID );
-				end );
-
-				SafeCall( function () -- Save function refs and hide all shown overlays
-					RSO = RareSpawnOverlay.API;
-					Show, Hide = assert( RSO.ShowNPC ), assert( RSO.HideNPC );
-
-					-- Remove all achievement overlays
-					for _, Achievement in pairs( _NPCScan.Achievements ) do
-						for _, NpcID in pairs( Achievement.Criteria ) do
-							Hide( RSO, NpcID );
-						end
-					end
-				end );
-			end
-		end );
-	end
 end
