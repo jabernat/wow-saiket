98a7b9e627f835bad9fb70013cc0decac43566e4
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2010-06-14 09:44:20 +0000

    _Underscore.Tooltip: Slight optimizations to unit tooltip updates, and the target-of-target tooltip now updates while shown.

--- trunk/_Underscore.Tooltip/_Underscore.Tooltip.Target.lua
+++ trunk/_Underscore.Tooltip/_Underscore.Tooltip.Target.lua
@@ -7,29 +7,53 @@
 local me = CreateFrame( "GameTooltip", "_UnderscoreTooltipTarget", GameTooltip, "GameTooltipTemplate" );
 _Underscore.Tooltip.Target = me;
 
+me.UpdateRate = 0.5;
+
 
 
 
 --[[****************************************************************************
+  * Function: _Underscore.Tooltip.Target:OnTooltipSetUnit                      *
+  ****************************************************************************]]
+function me:OnTooltipSetUnit ()
+	me.Text:SetTextColor( GameTooltip_UnitColor( "mouseovertarget" ) );
+end
+
+
+do
+	local NextUpdate = 0;
+--[[****************************************************************************
+  * Function: _Underscore.Tooltip.Target:OnTooltipCleared                      *
+  ****************************************************************************]]
+	function me:OnTooltipCleared ()
+		NextUpdate = 0;
+	end
+--[[****************************************************************************
   * Function: _Underscore.Tooltip.Target:OnUpdate                              *
   * Description: Shows and hides the mouseover target.                         *
   ****************************************************************************]]
-function me:OnUpdate ()
-	if ( self:IsUnit( "mouseover" ) and UnitExists( "mouseovertarget" ) ) then
-		if ( not me:IsShown() ) then
-			me:SetOwner( self, "ANCHOR_PRESERVE" );
-			me:SetUnit( "mouseovertarget" ); -- Shows the tooltip
-			me.Text:SetTextColor( GameTooltip_UnitColor( "mouseovertarget" ) );
+	local UnitExists = UnitExists;
+	function me:OnUpdate ( Elapsed )
+		NextUpdate = NextUpdate - Elapsed;
+		if ( NextUpdate <= 0 ) then
+			NextUpdate = me.UpdateRate;
+
+			if ( self:IsUnit( "mouseover" ) and UnitExists( "mouseovertarget" ) ) then
+				me:SetOwner( self, "ANCHOR_PRESERVE" );
+				me:SetUnit( "mouseovertarget" ); -- Shows the tooltip
+			else
+				me:Hide();
+			end
 		end
-	else
-		me:Hide();
 	end
 end
 
 
 
 
+GameTooltip:HookScript( "OnTooltipCleared", me.OnTooltipCleared );
 GameTooltip:HookScript( "OnUpdate", me.OnUpdate );
+me:SetScript( "OnTooltipSetUnit", me.OnTooltipSetUnit );
 
 me:SetPoint( "TOPLEFT", GameTooltip, "TOPRIGHT" );
 me:SetScale( 0.8 );
