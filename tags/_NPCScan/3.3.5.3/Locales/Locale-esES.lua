cff7b9e3a9bd23b6bcceda4d29c1e2a7df049da9
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2010-07-02 06:31:46 +0000

    _NPCScan: Tagged 3.3.5.3 release.

---
+++ tags/_NPCScan/3.3.5.3/Locales/Locale-esES.lua
@@ -0,0 +1,19 @@
+--[[****************************************************************************
+  * _NPCScan by Saiket                                                         *
+  * Locales/Locale-esES.lua - Localized string constants (es-ES/es-MX).        *
+  ****************************************************************************]]
+
+
+if ( GetLocale() ~= "esES" and GetLocale() ~= "esMX" ) then
+	return;
+end
+
+
+-- See http://wow.curseforge.com/addons/npcscan/localization/esES/
+_NPCScanLocalization.NPCS = setmetatable( {
+	[ 18684 ] = "Bro'Gaz sin Clan",
+	[ 32491 ] = "Protodraco Tiempo Perdido",
+	[ 33776 ] = "Gondria",
+	[ 35189 ] = "Skoll",
+	[ 38453 ] = "Arcturis",
+}, { __index = _NPCScanLocalization.NPCS; } );
