0c3c253b71474ee23402e07cf0454a1d63c3cab8
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2010-07-02 05:19:23 +0000

    GridStatusHealthFade: Replaced comments with LuaDoc syntax.

--- trunk/GridStatusHealthFade/Locales/Locale-enUS.lua
+++ trunk/GridStatusHealthFade/Locales/Locale-enUS.lua
@@ -4,18 +4,12 @@
   ****************************************************************************]]
 
 
-GridStatusHealthFadeLocalization = setmetatable( {
-	TITLE = "Health Fade";
+local L = AceLibrary( "AceLocale-2.2" ):new( "GridStatusHealthFade" );
+L:RegisterTranslations( "enUS", function() return {
 	COLOR_HIGH = "Color High";
 	COLOR_HIGH_DESC = "Color to blend for units at max health";
 	COLOR_LOW = "Color Low";
 	COLOR_LOW_DESC = "Color to blend for units at no health";
 	LABEL_FORMAT = "%d%%"; -- Format for health percentage labels
-}, {
-	__index = function ( self, Key )
-		if ( Key ~= nil ) then
-			rawset( self, Key, Key );
-			return Key;
-		end
-	end;
-} );
+	TITLE = "Health Fade";
+}; end );
