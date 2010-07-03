0e9b3601726fd28505f99b281e9d72bbce551fe1
Saiket <saiket.wow@gmail.com>
2010-07-03 01:12:48 +0000

    _UTF: Tagged 3.3.5.3 release.

---
+++ 3.3.5.3/_UTF.Chat.lua
@@ -0,0 +1,91 @@
+--[[****************************************************************************
+  * _UTF by Saiket                                                             *
+  * _UTF.Chat.lua - Text replacement hooks for macro commands and chat.        *
+  ****************************************************************************]]
+
+
+_UTFOptions.Chat = {
+	EntityReferenceReplace = true;
+	TextReplace = false;
+	TextReplacements = {
+		{ "%.%.%.+", _UTF.IntToUTF( 8230 ) } -- &hellip;
+	};
+};
+
+
+local _UTF = select( 2, ... );
+local me = {};
+_UTF.Chat = me;
+
+
+
+
+--- Replaces character references in a chat edit box.
+-- @return True if any replacements were made.
+function me:ReplaceEditBoxText ()
+	if ( _UTFOptions.Chat.EntityReferenceReplace ) then
+		local OldText = self:GetText();
+		local Command = OldText:match( "^(/%S+)" );
+		if ( not ( Command and IsSecureCmd( Command ) ) ) then
+			local NewText, CursorPosition = _UTF.ReplaceCharacterReferences( OldText, self:GetCursorPosition() );
+			if ( OldText ~= NewText ) then
+				self:SetText( NewText );
+				self:SetCursorPosition( CursorPosition );
+				return true;
+			end
+		end
+	end
+end
+
+
+do
+	--- Replaces the "Handled" return without affecting other returns.
+	local function HandleReturn ( self, Handled, ... )
+		return me.ReplaceEditBoxText( self ) or Handled, ...;
+	end
+	local Backup = ChatEdit_CustomTabPressed;
+	--- Replaces references when tab is pressed in an edit box.
+	-- @return True if the tab keypress was handled.
+	function me:ChatEditCustomTabPressed ( ... )
+		if ( not self ) then
+			self = this;
+		end
+		return HandleReturn( self, Backup( self, ... ) );
+	end
+end
+do
+	local Backup = SendChatMessage;
+	--- Replaces custom text strings in outbound chat.
+	function me.SendChatMessage ( Message, ... )
+		if ( _UTFOptions.Chat.TextReplace and Message ) then
+			for _, Replacement in ipairs( _UTFOptions.Chat.TextReplacements ) do
+				Message = Message:gsub( Replacement[ 1 ], Replacement[ 2 ] );
+			end
+		end
+
+		return Backup( Message, ... );
+	end
+end
+do
+	local Backup = BNSendWhisper;
+	--- Replaces custom text strings in outbound Battle.net RealID whispers.
+	function me.BNSendWhisper ( ID, Message, ... )
+		if ( _UTFOptions.Chat.TextReplace and Message ) then
+			for _, Replacement in ipairs( _UTFOptions.Chat.TextReplacements ) do
+				Message = Message:gsub( Replacement[ 1 ], Replacement[ 2 ] );
+			end
+		end
+
+		return Backup( ID, Message, ... );
+	end
+end
+
+
+
+
+-- Hook to catch macro lines as they're executed
+hooksecurefunc( MacroEditBox, "SetText", me.ReplaceEditBoxText );
+
+ChatEdit_CustomTabPressed = me.ChatEditCustomTabPressed;
+SendChatMessage = me.SendChatMessage;
+BNSendWhisper = me.BNSendWhisper;
