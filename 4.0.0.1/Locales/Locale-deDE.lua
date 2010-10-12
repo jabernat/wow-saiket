bf1591e09b14e12f22def610511448171536579b
Saiket <saiket.wow@gmail.com>
2010-10-12 02:01:35 +0000

    _Corpse: Tagged 4.0.0.1 release.

---
+++ 4.0.0.1/Locales/Locale-deDE.lua
@@ -0,0 +1,19 @@
+--[[****************************************************************************
+  * _Corpse by Saiket                                                          *
+  * Locales/Locale-deDE.lua - Localized string constants (de-DE).              *
+  ****************************************************************************]]
+
+
+if ( GetLocale() ~= "deDE" ) then
+	return;
+end
+
+
+-- See http://wow.curseforge.com/addons/corpse/localization/deDE/
+local _Corpse = select( 2, ... );
+_Corpse.L = setmetatable( {
+	CORPSE_PATTERN = "^Leichnam von ([^%s%p%d%c]+)$",
+	ENEMY_OFFLINE_PATTERN = "^Spieler '([^%s%p%d%c]+)' ist nicht auffindbar%.$",
+	FRIEND_ADDED_PATTERN = "^([^%s%p%d%c]+) zu Freundesliste hinzugefügt%.$",
+	FRIEND_REMOVED_PATTERN = "^([^%s%p%d%c]+) von Freundesliste entfernt%.$",
+}, { __index = _Corpse.L; } );
