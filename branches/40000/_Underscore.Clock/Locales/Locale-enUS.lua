17358cb3b0bffdc2fec97ab2cbadcc7f51c1e77c
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2010-10-12 05:20:11 +0000

    Deleted reintegrated 40000 branch.

--- branches/40000/_Underscore.Clock/Locales/Locale-enUS.lua
+++
@@ -1,9 +0,0 @@
---[[****************************************************************************
-  * _Underscore.Clock by Saiket                                                *
-  * Locales/Locale-enUS.lua - Localized string constants (en-US).              *
-  ****************************************************************************]]
-
-
-select( 2, ... ).L = setmetatable( {
-	TIME_FORMAT = "T%02d:%02d:%02d";
-}, getmetatable( _Underscore.L ) );
