d9f102b4f3ffdaf3144157e7ad054b447e894f6f
Saiket <saiket.wow@gmail.com>
2010-10-23 16:00:05 +0000

    _Corpse: Tagged 4.0.0.2 release.

---
+++ 4.0.0.2/Locales/Locale-koKR.lua
@@ -0,0 +1,19 @@
+--[[****************************************************************************
+  * _Corpse by Saiket                                                          *
+  * Locales/Locale-koKR.lua - Localized string constants (ko-KR).              *
+  ****************************************************************************]]
+
+
+if ( GetLocale() ~= "koKR" ) then
+	return;
+end
+
+
+-- See http://wow.curseforge.com/addons/corpse/localization/koKR/
+local _Corpse = select( 2, ... );
+_Corpse.L = setmetatable( {
+	CORPSE_PATTERN = "^([^%s%p%d%c]+)의 시체$",
+	ENEMY_OFFLINE_PATTERN = "^([^%s%p%d%c]+)님을 찾을 수 없습니다%.$",
+	FRIEND_ADDED_PATTERN = "^([^%s%p%d%c]+)님이 친구 목록에 등록되었습니다%.$",
+	FRIEND_REMOVED_PATTERN = "^([^%s%p%d%c]+)님이 친구 목록에서 삭제되었습니다%.$",
+}, { __index = _Corpse.L; } );
