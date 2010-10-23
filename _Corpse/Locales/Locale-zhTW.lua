007406a74a16b8a9d6b51f26fd01ffcd67c6ee1e
Saiket <saiket.wow@gmail.com>
2010-10-23 15:25:50 +0000

    _Corpse: Updated deDE, frFR, and zhTW translations for 4.0.1.

--- _Corpse/Locales/Locale-zhTW.lua
+++ _Corpse/Locales/Locale-zhTW.lua
@@ -14,5 +14,5 @@ _Corpse.L = setmetatable( {
 	CORPSE_PATTERN = "^([^%s%p%d%c]+)的屍體$",
 	ENEMY_OFFLINE_PATTERN = "^無法找到「([^%s%p%d%c]+)」$",
 	FRIEND_ADDED_PATTERN = "^([^%s%p%d%c]+)已被加入好友名單$",
-	FRIEND_REMOVED_PATTERN = "^([^%s%p%d%c]+)已被從好友名單中刪除$",
+	FRIEND_REMOVED_PATTERN = "^([^%s%p%d%c]+)已從好友名單中移除。$",
 }, { __index = _Corpse.L; } );
