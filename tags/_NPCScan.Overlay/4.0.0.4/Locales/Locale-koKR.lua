e47eeb537f2b1e371420b09f0da302eebb51f221
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2010-12-06 02:31:30 +0000

    _NPCScan.Overlay: Tagged 4.0.0.4 release.

---
+++ tags/_NPCScan.Overlay/4.0.0.4/Locales/Locale-koKR.lua
@@ -0,0 +1,37 @@
+--[[****************************************************************************
+  * _NPCScan.Overlay by Saiket                                                 *
+  * Locales/Locale-koKR.lua - Localized string constants (ko-KR).              *
+  ****************************************************************************]]
+
+
+if ( GetLocale() ~= "koKR" ) then
+	return;
+end
+
+
+-- See http://wow.curseforge.com/addons/npcscan-overlay/localization/koKR/
+local Overlay = select( 2, ... );
+Overlay.L = setmetatable( {
+	NPCs = setmetatable( {
+		[ 18684 ] = "외톨이 브로가즈",
+		[ 32491 ] = "잃어버린 시간의 원시비룡",
+		[ 33776 ] = "곤드리아",
+		[ 35189 ] = "스콜",
+		[ 38453 ] = "아크튜리스",
+	}, { __index = Overlay.L.NPCs; } );
+
+	CONFIG_ALPHA = "투명도",
+	CONFIG_DESC = "맵에 희귀몹의 이동경로를 어떻게 표시할것인지 설정할 수 있습니다. 여러 월드맵 관련 애드온에서도 사용이 가능합니다.",
+	CONFIG_SHOWALL = "항상 모든 이동경로 표시",
+	CONFIG_SHOWALL_DESC = "기본적으로 희귀몹을 탐색하지 않았을때는 경로를 표시하지 않지만, 이 옵션을 사용하면 항상 이동경로를 표시합니다.",
+	CONFIG_TITLE = "이동경로 표시",
+	CONFIG_TITLE_STANDALONE = "_|cffCCCC88NPCScan|r.Overlay",
+	MODULE_ALPHAMAP3 = "AlphaMap3 애드온",
+	MODULE_BATTLEFIELDMINIMAP = "지역 지도",
+	MODULE_MINIMAP = "미니맵",
+	MODULE_RANGERING_DESC = "이 링은 희귀몹이 출현하는 지역에서만 사용됩니다.",
+	MODULE_RANGERING_FORMAT = "미니맵에 %dyd의 탐색거리를 나타내는 링 표시",
+	MODULE_WORLDMAP = "세계 지도",
+	MODULE_WORLDMAP_KEY_FORMAT = "- %s",
+	MODULE_WORLDMAP_TOGGLE_DESC = "이 옵션을 활성화하면, 탐색하려는 NPC의 이동경로를 보여줍니다.",
+}, { __index = Overlay.L; } );
---
+++ tags/_NPCScan.Overlay/4.0.0.4/Locales/Locale-ruRU.lua
@@ -0,0 +1,20 @@
+--[[****************************************************************************
+  * _NPCScan.Overlay by Saiket                                                 *
+  * Locales/Locale-ruRU.lua - Localized string constants (ru-RU).              *
+  ****************************************************************************]]
+
+
+if ( GetLocale() ~= "ruRU" ) then
+	return;
+end
+
+
+-- See http://wow.curseforge.com/addons/npcscan-overlay/localization/ruRU/
+local Overlay = select( 2, ... );
+Overlay.L.NPCs = setmetatable( {
+	[ 18684 ] = "Бро'Газ Без Клана",
+	[ 32491 ] = "Затерянный во времени протодракон",
+	[ 33776 ] = "Гондрия",
+	[ 35189 ] = "Сколл",
+	[ 38453 ] = "Арктур",
+}, { __index = Overlay.L.NPCs; } );
