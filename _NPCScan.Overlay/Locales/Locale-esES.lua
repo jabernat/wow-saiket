a76a5dcb6fd6bf831895239e60c3acd5ac3eb969
Saiket <saiket.wow@gmail.com>
2010-12-06 00:21:15 +0000

    _NPCScan.Overlay: Updated deDE localization, and added esES/esMX localization by galen2.

--- _NPCScan.Overlay/Locales/Locale-esES.lua
+++ _NPCScan.Overlay/Locales/Locale-esES.lua
@@ -11,14 +11,32 @@ end
 
 -- See http://wow.curseforge.com/addons/npcscan-overlay/localization/esES/
 local Overlay = select( 2, ... );
-Overlay.L.NPCs = setmetatable( {
-	[ 1140 ] = "Matriarca Tajobuche",
-	[ 5842 ] = "Takk el Saltarín",
-	[ 6581 ] = "Matriarca ravasaurio",
-	[ 14232 ] = "Dardo",
-	[ 18684 ] = "Bro'Gaz sin Clan",
-	[ 32491 ] = "Protodraco Tiempo Perdido",
-	[ 33776 ] = "Gondria",
-	[ 35189 ] = "Skoll",
-	[ 38453 ] = "Arcturis",
-}, { __index = Overlay.L.NPCs; } );
+Overlay.L = setmetatable( {
+	NPCs = setmetatable( {
+		[ 1140 ] = "Matriarca Tajobuche",
+		[ 5842 ] = "Takk el Saltarín",
+		[ 6581 ] = "Matriarca ravasaurio",
+		[ 14232 ] = "Dardo",
+		[ 18684 ] = "Bro'Gaz sin Clan",
+		[ 32491 ] = "Protodraco Tiempo Perdido",
+		[ 33776 ] = "Gondria",
+		[ 35189 ] = "Skoll",
+		[ 38453 ] = "Arcturis",
+	}, { __index = Overlay.L.NPCs; } );
+
+	CONFIG_ALPHA = "Alpha",
+	CONFIG_DESC = "Controla en que mapas se mostrara la sobreposicion del camino del NPC. La mayoria de Addons de mapas se controlan con las opciones del mapa del mundo.",
+	CONFIG_SHOWALL = "Mostrar siempre todos los caminos",
+	CONFIG_SHOWALL_DESC = "Normalmente cuando no estas buscando un NPC, su camino se elimina del mapa, Activa esta opcion para mostrar siempre todos los caminos conocidos",
+	CONFIG_TITLE = "Sobreposicion",
+	CONFIG_TITLE_STANDALONE = "_|cffCCCC88NPCScan|r.Overlay",
+	MODULE_ALPHAMAP3 = "AddOn AlphaMap3",
+	MODULE_BATTLEFIELDMINIMAP = "Minimapa de campo de batalla",
+	MODULE_MINIMAP = "Minimapa",
+	MODULE_RANGERING_DESC = "Nota: el circulo de rango solo aparece en zonas donde se pueden buscar NPCs raros",
+	MODULE_RANGERING_FORMAT = "Muestra un anillo de %dyd para aproximar el rango de busqueda",
+	MODULE_WORLDMAP = "Mapa del mundo principal",
+	MODULE_WORLDMAP_KEY_FORMAT = "• %s",
+	MODULE_WORLDMAP_TOGGLE = "NPCs",
+	MODULE_WORLDMAP_TOGGLE_DESC = "Activa/desactiva los caminos de los NPCs en _|cffCCCC88NPCScan|r.Overlay.",
+}, { __index = Overlay.L; } );
