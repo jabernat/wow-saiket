ad8d14c558d393785dd288cb03894b289322afa0
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2011-01-25 03:15:59 +0000

    _Corpse: Fixed broken koKR system message patterns.

--- trunk/_Corpse/Locales/Locale-koKR.lua
+++ trunk/_Corpse/Locales/Locale-koKR.lua
@@ -13,7 +13,7 @@ end
 local _Corpse = select( 2, ... );
 _Corpse.L = setmetatable( {
 	CORPSE_PATTERN = "^([^%s%p%d%c]+)의 시체$",
-	ENEMY_OFFLINE_PATTERN = "^([^%s%p%d%c]+)님을 찾을 수 없습니다%.$",
-	FRIEND_ADDED_PATTERN = "^([^%s%p%d%c]+)님이 친구 목록에 등록되었습니다%.$",
-	FRIEND_REMOVED_PATTERN = "^([^%s%p%d%c]+)님이 친구 목록에서 삭제되었습니다%.$",
+	ENEMY_OFFLINE_PATTERN = "^([^%s%p%d%c]+) 님을 찾을 수 없습니다%.$",
+	FRIEND_ADDED_PATTERN = "^([^%s%p%d%c]+) 님이 친구 목록에 등록되었습니다%.$",
+	FRIEND_REMOVED_PATTERN = "^([^%s%p%d%c]+) 님이 친구 목록에서 삭제되었습니다%.$",
 }, { __index = _Corpse.L; } );
