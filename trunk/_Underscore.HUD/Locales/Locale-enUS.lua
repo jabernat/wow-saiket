18eb03c2a6fc3d619da64418dfae16e7e4c33acb
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2011-04-26 12:57:12 +0000

    _Underscore.HUD: Removed the Push-to-Talk indicator, and redesigned the AFK/DND indicator to use an animation timer.

--- trunk/_Underscore.HUD/Locales/Locale-enUS.lua
+++ trunk/_Underscore.HUD/Locales/Locale-enUS.lua
@@ -11,15 +11,4 @@ select( 2, ... ).L = setmetatable( {
 	FEIGN   = "FEIGN";
 
 	VALUE_IGNORED = "N/A";
-
-	PUSHTOTALK_BIND = "_|cffCCCC88Underscore|r.HUD.PushToTalk: Binding set to |cff808080“%s”|r.";
-	PUSHTOTALK_BIND_ERROR = "_|cffCCCC88Underscore|r.HUD.PushToTalk: Error attempting to bind to |cff808080“%s”|r.";
 }, getmetatable( _Underscore.L ) );
-
-
-BINDING_HEADER__UNDERSCORE_HUD = "_|cffCCCC88Underscore|r.HUD";
-BINDING_NAME__UNDERSCORE_HUD_PUSHTOTALK = "External Push-to-Talk Key";
-
-SLASH__UNDERSCORE_HUD_PUSHTOTALK1 = "/ptt";
-SLASH__UNDERSCORE_HUD_PUSHTOTALK3 = "/uptt";
-SLASH__UNDERSCORE_HUD_PUSHTOTALK2 = "/underscoreptt";
