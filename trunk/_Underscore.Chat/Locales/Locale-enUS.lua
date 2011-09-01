a64306bb5742ed4aa090095a007b935a18a83193
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2011-09-01 02:40:27 +0000

    _Underscore.Chat: Fixed abbreviated chat type link for raid warnings.

--- trunk/_Underscore.Chat/Locales/Locale-enUS.lua
+++ trunk/_Underscore.Chat/Locales/Locale-enUS.lua
@@ -18,20 +18,20 @@ select( 2, ... ).L = setmetatable( {
 
 
 -- Chat message formats
-CHAT_BATTLEGROUND_GET = "|Hchannel:Battleground|h[B]|h %s: ";
-CHAT_BATTLEGROUND_LEADER_GET = [[|Hchannel:Battleground|h[B|TInterface\GroupFrame\UI-Group-LeaderIcon:0|t]|h %s: ]];
-CHAT_GUILD_GET = "|Hchannel:Guild|h[G]|h %s: ";
-CHAT_OFFICER_GET = "|Hchannel:Officer|h[O]|h %s: ";
-CHAT_PARTY_GET = "|Hchannel:Party|h[P]|h %s: ";
-CHAT_PARTY_LEADER_GET = [[|Hchannel:Party|h[P|TInterface\GroupFrame\UI-Group-LeaderIcon:0|t]|h %s: ]];
+CHAT_BATTLEGROUND_GET = "|Hchannel:BATTLEGROUND|h[B]|h %s: ";
+CHAT_BATTLEGROUND_LEADER_GET = [[|Hchannel:BATTLEGROUND|h[B|TInterface\GroupFrame\UI-Group-LeaderIcon:0|t]|h %s: ]];
+CHAT_GUILD_GET = "|Hchannel:GUILD|h[G]|h %s: ";
+CHAT_OFFICER_GET = "|Hchannel:OFFICER|h[O]|h %s: ";
+CHAT_PARTY_GET = "|Hchannel:PARTY|h[P]|h %s: ";
+CHAT_PARTY_LEADER_GET = [[|Hchannel:PARTY|h[P|TInterface\GroupFrame\UI-Group-LeaderIcon:0|t]|h %s: ]];
 CHAT_PARTY_GUIDE_GET = CHAT_PARTY_LEADER_GET;
-CHAT_RAID_GET = "|Hchannel:Raid|h[R]|h %s: ";
-CHAT_RAID_LEADER_GET = [[|Hchannel:Raid|h[R|TInterface\GroupFrame\UI-Group-LeaderIcon:0|t]|h %s: ]];
-CHAT_RAID_WARNING_GET = "|Hchannel:RaidWarning|h[R-WARN]|h %s: ";
-CHAT_SAY_GET = "|Hchannel:Say|h[S]|h %s: ";
+CHAT_RAID_GET = "|Hchannel:RAID|h[R]|h %s: ";
+CHAT_RAID_LEADER_GET = [[|Hchannel:RAID|h[R|TInterface\GroupFrame\UI-Group-LeaderIcon:0|t]|h %s: ]];
+CHAT_RAID_WARNING_GET = "|Hchannel:RW|h[R-WARN]|h %s: ";
+CHAT_SAY_GET = "|Hchannel:SAY|h[S]|h %s: ";
 CHAT_WHISPER_GET = "[W] %s: ";
 CHAT_WHISPER_INFORM_GET = "[W]»%s: ";
-CHAT_YELL_GET = "|Hchannel:Yell|h[Y]|h %s: ";
+CHAT_YELL_GET = "|Hchannel:YELL|h[Y]|h %s: ";
 
 CHAT_MONSTER_PARTY_GET   = CHAT_PARTY_GET;
 CHAT_MONSTER_SAY_GET     = CHAT_SAY_GET;
