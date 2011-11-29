ea5cdc7e73a9f9f726797f9f9ca8189d03f34d8e
Saiket <saiket.wow@gmail.com>
2011-11-29 22:02:28 +0000

    GuildBankSearch: Tagged 4.3.0.1 release.

---
+++ 4.3.0.1/Locales/Locale-zhTW.lua
@@ -0,0 +1,26 @@
+--[[****************************************************************************
+  * GuildBankSearch by Saiket, originally by Harros                            *
+  * Locales/Locale-zhTW.lua - Localized string constants (zhTW)                *
+  ****************************************************************************]]
+
+
+if ( GetLocale() ~= "enTW" and GetLocale() ~= "zhTW" ) then
+	return;
+end
+
+
+-- See http://wow.curseforge.com/addons/guild-bank-search/localization/zhTW/
+local GuildBankSearch = select( 2, ... );
+GuildBankSearch.L = setmetatable( {
+	ALL = "|cffcccccc(所有)|r",
+	CLEAR = "清除",
+	FILTER = "物品搜索",
+	ITEM_CATEGORY = "分類物品",
+	ITEM_LEVEL = "物品等級:",
+	NAME = "名稱:",
+	QUALITY = "品質:",
+	REQUIRED_LEVEL = "需要等級:",
+	SLOT = "欄位:",
+	SUB_TYPE = "副類別:",
+	TYPE = "類別:",
+}, { __index = GuildBankSearch.L; } );
