54ac99e9b897318e5324267943b22bc2281bfe21
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2011-12-14 03:00:40 +0000

    _Underscore.Units.Arena: Now unregisters unit watch state drivers after leaving arenas.

--- trunk/_Underscore.Units.Arena/_Underscore.Units.Arena.lua
+++ trunk/_Underscore.Units.Arena/_Underscore.Units.Arena.lua
@@ -5,44 +5,75 @@
 
 
 local _Underscore = _Underscore;
-local NS = _Underscore.Units.oUF;
-assert( NS.StyleMeta.__call, "_Underscore.Units.oUF Initializer already destroyed!" );
+local Units = _Underscore.Units.oUF;
+assert( Units.StyleMeta.__call, "_Underscore.Units.oUF Initializer already destroyed!" );
+local NS = CreateFrame( "Frame" );
+Units.Arena = NS;
+
+NS.Enabled = false;
+NS.ArenaTargets = {}; -- [ ArenaFrame ] = ArenaTargetFrame;
+
+
+
+
+--- Enables or disables unit watch state drivers when entering or leaving arenas.
+function NS:PLAYER_ENTERING_WORLD ()
+	local Enabled = select( 2, IsInInstance() ) == "arena";
+	if ( self.Enabled == Enabled ) then
+		return;
+	end
+	self.Enabled = Enabled;
+
+	for Frame, Target in pairs( self.ArenaTargets ) do
+		if ( Enabled ) then
+			Frame:Enable();
+			-- Show only when target is friendly
+			RegisterStateDriver( Target, "visibility", "[target="..Target.unit..",help]show;hide" );
+		else
+			Frame:Disable();
+			Target:Disable();
+		end
+	end
+end
+
+
+
 
 oUF:RegisterStyle( "_UnderscoreUnitsArena", setmetatable( {
 	Width = 110;
 	Height = 36;
 	HealthText = "Tiny";
 	PowerText  = "Tiny";
-	NameFont = NS.FontTiny;
+	NameFont = Units.FontTiny;
 	CastTime = false;
 	Auras = false;
 	DebuffHighlight = false;
 	PowerHeight = 0.2;
 	ProgressHeight = 0.2;
-}, NS.StyleMeta ) );
+}, Units.StyleMeta ) );
 oUF:RegisterStyle( "_UnderscoreUnitsArenaTarget", setmetatable( {
 	Width = 80;
 	Height = 36;
 	PortraitSide = "LEFT";
 	HealthText = false;
 	PowerText  = false;
-	NameFont = NS.FontTiny;
+	NameFont = Units.FontTiny;
 	CastTime = false;
 	Auras = false;
 	PowerHeight = 0.2;
 	ProgressHeight = 0.2;
-}, NS.StyleMeta ) );
-
+}, Units.StyleMeta ) );
 
 local LastFrame;
 for Index = 1, 5 do
 	oUF:SetActiveStyle( "_UnderscoreUnitsArena" );
 	local Frame = oUF:Spawn( "arena"..Index, "_UnderscoreUnitsArena"..Index );
+	Frame:Disable();
 	if ( LastFrame ) then
 		Frame:SetPoint( "TOPLEFT", LastFrame, "BOTTOMLEFT", 0, -_Underscore.Backdrop.Padding * 2 );
 	else
 		Frame:SetPoint( "LEFT", UIParent );
-		Frame:SetPoint( "TOP", NS.Pet, "BOTTOM", 0, -185 );
+		Frame:SetPoint( "TOP", Units.Pet, "BOTTOM", 0, -185 );
 	end
 	LastFrame = Frame;
 
@@ -50,10 +81,14 @@ for Index = 1, 5 do
 	oUF:SetActiveStyle( "_UnderscoreUnitsArenaTarget" );
 	local Target = oUF:Spawn( Frame.unit.."target", Frame:GetName().."Target" );
 	Target:SetPoint( "TOPLEFT", Frame, "TOPRIGHT", _Underscore.Backdrop.Padding * 2, 0 );
-	-- Show only when target is friendly
-	UnregisterUnitWatch( Target );
-	RegisterStateDriver( Target, "visibility", "[target="..Target.unit..",help]show;hide" );
+	Target:Disable();
+
+	NS.ArenaTargets[ Frame ] = Target;
 end
 
 -- Garbage collect _Underscore.Units.oUF's initialization code
-NS.StyleMeta.__call = nil;
+Units.StyleMeta.__call = nil;
+
+NS:SetScript( "OnEvent", _Underscore.Frame.OnEvent );
+NS:RegisterEvent( "PLAYER_ENTERING_WORLD" );
+NS:PLAYER_ENTERING_WORLD();
