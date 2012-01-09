242cf898ad0b35260bbd8db7701a21f5b942583f
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2012-01-09 06:03:34 +0000

    Deleted unnecessary 4.3.2 PTR branch.

--- branches/4.3.2 PTR/_Underscore.Chat.CombatLog/_Underscore.Chat.CombatLog.lua
+++
@@ -1,8 +0,0 @@
---[[****************************************************************************
-  * _Underscore.Chat.CombatLog by Saiket                                       *
-  * _Underscore.Chat.CombatLog.lua - Modifies the Blizzard_CombatLog addon.    *
-  ****************************************************************************]]
-
-
--- Hide gray header texture above combat log
-CombatLogQuickButtonFrame_CustomTexture:Hide();
--- branches/4.3.2 PTR/_Underscore.Tooltip/Locales/Locale-enUS.lua
+++
@@ -1,9 +0,0 @@
---[[****************************************************************************
-  * _Underscore.Tooltip by Saiket                                              *
-  * Locales/Locale-enUS.lua - Localized string constants (en-US).              *
-  ****************************************************************************]]
-
-
-select( 2, ... ).L = setmetatable( {
-	GUILD_FORMAT = "<%s>"; -- GuildName
-}, getmetatable( _Underscore.L ) );
