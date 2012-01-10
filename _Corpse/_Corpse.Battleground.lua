c90e747c9cfadeb28812907538c4bd393494c776
Saiket <saiket.wow@gmail.com>
2012-01-10 03:47:23 +0000

    _Corpse: Battleground module now determines player faction more reliably.
    GetBattlefieldArenaFaction seems to return a faction group ID matching the player's GetBattlefieldScore result, even when factions are switched in wargames/rated BGs/arenas.

--- _Corpse/_Corpse.Battleground.lua
+++ _Corpse/_Corpse.Battleground.lua
@@ -13,28 +13,24 @@ local MIN_REQUEST_INTERVAL = 5; -- Seconds to keep cached scoreboard data
 
 
 
-do
-	local FACTION_PLAYER = ( { -- Faction group ID matching GetBattlefieldScore's return
-		Horde = 0; Alliance = 1;
-	} )[ UnitFactionGroup( "player" ) ];
-	--- Scans the scoreboard for a given player.
-	-- @return Arguments for BuildTooltip, similar to GetFriendInfo.
-	-- @see _Corpse:BuildTooltip
-	function NS:GetBattlefieldInfo ( Name )
-		for Index = 1, GetNumBattlefieldScores() do
-			local NameServer, _, _, _, _, Faction, _, Class = GetBattlefieldScore( Index );
-			local NameBG = NameServer:match( "^[^-]+" ); -- Discard server name
+--- Scans the scoreboard for a given player.
+-- @return Arguments for BuildTooltip, similar to GetFriendInfo.
+-- @see _Corpse:BuildTooltip
+function NS:GetBattlefieldInfo ( Name )
+	local FactionPlayer = GetBattlefieldArenaFaction();
+	for Index = 1, GetNumBattlefieldScores() do
+		local NameServer, _, _, _, _, Faction, _, Class = GetBattlefieldScore( Index );
+		local NameBG = NameServer:match( "^[^-]+" ); -- Discard server name
 
-			if ( Name == NameBG ) then
-				local Hostile = Faction ~= FACTION_PLAYER;
-				if ( Hostile or _Corpse:UnitHasCorpse( NameServer ) ) then -- Ignore name matches for living allies
-					local Connected, Level, Status = 1;
-					if ( not Hostile ) then -- Get extra details for allies in raid
-						Level, Connected = UnitLevel( NameServer ), UnitIsConnected( NameServer ) and 1 or 0;
-						Status = PlayerIsPVPInactive( NameServer ) and CHAT_FLAG_AFK;
-					end
-					return Hostile, Name, Level, Class, GetRealZoneText(), Connected, Status;
+		if ( Name == NameBG ) then
+			local Hostile = Faction ~= FactionPlayer;
+			if ( Hostile or _Corpse:UnitHasCorpse( NameServer ) ) then -- Ignore name matches for living allies
+				local Connected, Level, Status = 1;
+				if ( not Hostile ) then -- Get extra details for allies in raid
+					Level, Connected = UnitLevel( NameServer ), UnitIsConnected( NameServer ) and 1 or 0;
+					Status = PlayerIsPVPInactive( NameServer ) and CHAT_FLAG_AFK;
 				end
+				return Hostile, Name, Level, Class, GetRealZoneText(), Connected, Status;
 			end
 		end
 	end
