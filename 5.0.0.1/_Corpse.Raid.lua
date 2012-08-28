fb0da7d2d65c8599147eb53a07adcef88c7191e7
Saiket <saiket.wow@gmail.com>
2012-08-28 03:37:06 +0000

    _Corpse: Tagged 5.0.0.1 release.

---
+++ 5.0.0.1/_Corpse.Raid.lua
@@ -0,0 +1,18 @@
+--[[****************************************************************************
+  * _Corpse by Saiket                                                          *
+  * _Corpse.Raid.lua - Uses raid member data to get corpse info.               *
+  ****************************************************************************]]
+
+
+local _Corpse = select( 2, ... );
+local NS = {};
+_Corpse.Raid = NS;
+
+
+
+
+--- Returns an iterator for all raid UnitIDs.
+function NS:IterateUnitIDs ()
+	-- Last ID is always player, so skip it
+	return _Corpse.NextUnitID, "raid", GetNumGroupMembers() - 1;
+end
