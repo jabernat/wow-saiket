d36c0b1618821ab073727df1d163339a726c38df
saiket.wow <saiket.wow@14be28a3-514c-0410-8076-454f53b1eec0>
2012-08-28 04:06:08 +0000

    GuildBankSearch: Tagged 5.0.0.1 release.

---
+++ tags/GuildBankSearch/5.0.0.1/Locales/Locale-enUS.lua
@@ -0,0 +1,30 @@
+--[[****************************************************************************
+  * GuildBankSearch by Saiket, originally by Harros                            *
+  * Locales/Locale-enUS.lua - Localized string constants (en-US).              *
+  ****************************************************************************]]
+
+
+-- See http://wow.curseforge.com/addons/guild-bank-search/localization/enUS/
+select( 2, ... ).L = setmetatable( {
+	ALL = "|cffcccccc(All)|r",
+	CLEAR = "Clear",
+	FILTER = "Filter",
+	ITEM_CATEGORY = "Item Category",
+	ITEM_LEVEL = "Item Level:",
+	LEVELRANGE_SEPARATOR = "-",
+	NAME = "Name:",
+	QUALITY = "Quality:",
+	REQUIRED_LEVEL = "Required Level:",
+	SLOT = "Slot:",
+	SUB_TYPE = "Sub-Type:",
+	TEXT = "Tooltip Text:",
+	TITLE = "GuildBank|cffccccccSearch|r",
+	TYPE = "Type:",
+}, {
+	__index = function ( self, Key )
+		if ( Key ~= nil ) then
+			rawset( self, Key, Key );
+			return Key;
+		end
+	end;
+} );
